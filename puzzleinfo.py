9aae4352fddb23479310f894ed557e9463c56b07
Mikhael Goikhman <migo@freeshell.org>
2025-01-30 20:33:01 +0200

    tools, web: Update index, puzzle and theme pages to include screenshots
    Add 5 main screenshots unto the index html page.
    
    Add front screenshot into the puzzle index html page.
    
    Add all puzzle screenshots to each puzzle html page.
    
    Add all theme screenshots to each theme html page.
    
    Also, simplify puzzleinfo.py, no need to have get_field methods.
    
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- puzzleinfo.py
+++ puzzleinfo.py
@@ -14,46 +14,38 @@ class PuzzleInfo:
 		self.filename = "%s/%s.txt" % (PUZZLE_INFO_DIR, name)
 		if not os.path.isfile(self.filename):
 			self.die("No required file %s found" % self.filename)
+
 		file = open(self.filename, "r")
+
 		self.title = file.readline().rstrip("\n")
 		if self.title == "\n" or file.readline() != "\n":
 			self.die("Invalid format: first line must be title, second line empty")
 
+		self.description = ""
 		self.goal = "Unspecified Goal"
-		self.paragraphs = []
-		paragraph = ""
+
 		while line := file.readline():
-			line = line.rstrip("\n")
+			line = line.strip()
 			if line.startswith("Goal: "):
 				self.goal = line[6:]
-			elif line == "":
-				if paragraph != "":
-					self.paragraphs.append(paragraph)
-					paragraph = ""
 			else:
-				if paragraph != "":
-					paragraph += "\n"
-				paragraph += line
-		if paragraph != "":
-			self.paragraphs.append(paragraph)
+				self.description += line + "\n"
 
-	def get_title(self):
-		return self.title
+		# clean up description, trim and leave 2 endlines at most
+		self.description = self.description.strip()
+		while '\n\n\n' in self.description:
+			self.description = self.description.replace("\n\n\n", "\n\n")
 
-	def get_description(self):
-		return "\n\n".join(self.paragraphs)
-
-	def get_goal(self):
-		return self.goal
+		file.close()
 
 	def get_hypertext(self):
 		return "### %s\n\n%s\n\nGoal: %s\n\n" % \
-			(self.get_title(), self.get_description(), self.get_goal())
+			(self.title, self.description, self.goal)
 
 	def get_html(self):
-		return '<h1>%s</h1>\n\n<p>%s\n</p>\n\n<p class="goal">%s</p>' % \
-			(self.get_title(), "\n</p>\n\n<p>".join(self.paragraphs), self.get_goal())
+		return '<h1>%s</h1>\n\n<!--SCREENSHOTS-->\n<p>%s\n</p>\n\n<p class="goal">%s</p>' % \
+			(self.title, "\n</p>\n\n<p>".join(self.description.split("\n\n")), self.goal)
 
-def get_all_puzzleinfo_objects():
+def get_all_puzzle_infos():
 	all_names = get_dir_names(PUZZLE_INFO_DIR, ".txt")
 	return (PuzzleInfo(name) for name in all_names)
