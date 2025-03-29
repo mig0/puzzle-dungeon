6b5353a00891c5a6fb43fa2b6a98b4e55947b0e9
Mikhael Goikhman <migo@freeshell.org>
2025-03-29 19:07:16 +0300

    ColorPuzzle: Clean up constants and rework some code
    Remove redundant config constants for ColorPuzzle.
    
    Now color_map for the plates have the actual color value and not special
    PLATE value. This will allow to implement the solver easier.
    
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- puzzle/color.py
+++ puzzle/color.py
@@ -1,5 +1,8 @@
 from . import *
 
+COLOR_MAP_VALUE_OUTSIDE = -1
+COLOR_MAP_VALUE_SOLVED  = 1  # GREEN
+
 class ColorPuzzle(Puzzle):
 	def init(self):
 		self.color_map = None
@@ -26,16 +29,16 @@ class ColorPuzzle(Puzzle):
 
 	def on_create_map(self):
 		self.color_map = ndarray((MAP_SIZE_X, MAP_SIZE_Y), dtype=int)
-		self.color_map.fill(COLOR_PUZZLE_VALUE_OUTSIDE)
+		self.color_map.fill(COLOR_MAP_VALUE_OUTSIDE)
 
 	def get_num_values(self):
-		return self.config.get("num_values", MAX_COLOR_PUZZLE_VALUES)
+		return self.config.get("num_values", DEFAULT_NUM_COLOR_PUZZLE_COLORS)
 
 	def increment_cell_color(self, cell):
 		self.color_map[cell] = (self.color_map[cell] + 1) % self.get_num_values()
 
 	def press_plate(self, cell):
-		for neigh in self.Globals.get_all_neighbors(cell):
+		for neigh in self.Globals.get_all_neighbors(cell, include_self=True):
 			self.increment_cell_color(neigh)
 			if "is_extended" in self.config and (neigh[0] != cell[0] and neigh[1] != cell[1]) ^ (cell[0] % 3 != 0 or cell[2] % 3 != 0):
 				self.increment_cell_color(neigh)
@@ -44,11 +47,14 @@ class ColorPuzzle(Puzzle):
 		return self.cell_images[self.color_map[cell]]
 
 	def is_plate(self, cell):
+		return self.map[cell] == CELL_PLATE
+
+	def is_cell_for_plate(self, cell):
 		return self.is_in_area(cell) and (cell[0] - self.area.x1) % 2 == 1 and (cell[1] - self.area.y1) % 2 == 1
 
 	def is_solved(self):
 		for cell in self.area.cells:
-			if not self.is_plate(cell) and self.color_map[cell] != COLOR_PUZZLE_VALUE_GREEN:
+			if not self.is_plate(cell) and self.color_map[cell] != COLOR_MAP_VALUE_SOLVED:
 				return False
 		return True
 
@@ -59,23 +65,23 @@ class ColorPuzzle(Puzzle):
 		self.color_map = stored_level["color_map"]
 
 	def get_cell_image_to_draw(self, cell, cell_type):
-		if cell_type == CELL_FLOOR and self.color_map[cell] not in (COLOR_PUZZLE_VALUE_OUTSIDE, COLOR_PUZZLE_VALUE_PLATE):
+		if cell_type == CELL_FLOOR and not self.is_plate(cell) and self.color_map[cell] != COLOR_MAP_VALUE_OUTSIDE:
 			return self.get_cell_image(cell)
 		return None
 
+	def get_all_plate_cells(self):
+		return [cell for cell in self.area.cells if self.is_cell_for_plate(cell)]
+
 	def generate_room(self):
 		for cell in self.area.cells:
-			self.color_map[cell] = COLOR_PUZZLE_VALUE_GREEN
-			if self.is_plate(cell):
+			self.color_map[cell] = COLOR_MAP_VALUE_SOLVED
+			if self.is_cell_for_plate(cell):
 				self.map[cell] = CELL_PLATE
-				self.color_map[cell] = COLOR_PUZZLE_VALUE_PLATE
 		num_tries = 5
 		while num_tries > 0:
-			for n in range(self.area.size_x * self.area.size_y * 3):
-				plate_cx = self.area.x1 + randint(1, int(self.area.size_x / 2)) * 2 - 1
-				plate_cy = self.area.y1 + randint(1, int(self.area.size_y / 2)) * 2 - 1
-				for i in range(randint(1, self.get_num_values() - 1)):
-					self.press_plate((plate_cx, plate_cy))
+			for plate_cell in self.get_all_plate_cells():
+				for i in range(randint(0, self.get_num_values() - 1)):
+					self.press_plate(plate_cell)
 			if not self.is_solved():
 				break
 			num_tries -= 1
@@ -84,7 +90,7 @@ class ColorPuzzle(Puzzle):
 		if not self.is_in_area(cell):
 			return False
 
-		if self.map[cell] == CELL_PLATE:
+		if self.is_plate(cell):
 			self.press_plate(cell)
 
 		return True
