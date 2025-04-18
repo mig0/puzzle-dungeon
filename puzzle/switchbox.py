7c849b1fd0388525580b25afd42b6c2b130e5a9b
Mikhael Goikhman <migo@freeshell.org>
2025-04-18 18:13:54 +0300

    SwitchBoxPuzzle: Add color-based visuals with toggle and theming support
    Introduce coloring to visually distinguish groups of gates, plates and
    barrels based on their logical connections.
    
    Assign consistent colors to plates and to objects (gates and barrels)
    that these plates trigger. Currently 15 main colors are used and they are
    rotated if there are more groups.
    
    The colors are added to plates and gates by converting the floor image to
    grayscale and then lightening it and filling the required color into it.
    The colors are added to barrels by simply filling the required color over
    the barrel's transparent image.
    
    This way all themes are supported. Although sometimes such colorizing may
    be confusing. For example, if the stock barrel is yellow, and the used
    group color is yellow, then the regular barrel (not triggered by any
    plate) may become indistinguished from the colored barrel.
    
    Allow to toggle colors using 'c' key. By default coloring is turned on
    unless the "hide_colors" config is set.
    
    Display a hint about pressing 'c' key to toggle colors on enter room.
    
    This improves gameplay clarity by visually grouping related gates, plates and
    barrels, and offers user control over the visual assistance.
    
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- puzzle/switchbox.py
+++ puzzle/switchbox.py
@@ -1,8 +1,10 @@
 from . import *
+import pygame
 
 class SwitchBoxPuzzle(Puzzle):
 	def init(self):
 		self.load_map_special_cell_types[CELL_PLATE] = 'ints'
+		self.hide_colors = self.config.get("hide_colors", False)
 
 	def has_plate(self):
 		return True
@@ -12,6 +14,88 @@ class SwitchBoxPuzzle(Puzzle):
 
 	def has_finish(self):
 		return True
+
+	def get_object_color_idx(self, plate_idxs):
+		if not plate_idxs:
+			return None
+		plate_idx = plate_idxs[0]
+		color_idx = self.plate_color_idxs[plate_idx]
+		if color_idx is None:
+			return None
+		return color_idx
+
+	def get_plate_color_image(self, plate_idx):
+		if type(plate_idx) == tuple:
+			plate_idx = self.plate_cells.index(plate_idx)
+		color_idx = self.plate_color_idxs[plate_idx]
+		if color_idx is None:
+			return None
+		return self.color_floor_images[color_idx]
+
+	def get_gate_color_image(self, gate_idx):
+		if type(gate_idx) == tuple:
+			gate_idx = self.gate_cells.index(gate_idx)
+		color_idx = self.get_object_color_idx(self.attached_gate_plate_idxs[gate_idx])
+		if color_idx is None:
+			return None
+		return self.color_floor_images[color_idx]
+
+	def get_barrel_color(self, barrel_idx):
+		if type(barrel_idx) == CellActor:
+			barrel_idx = barrels.index(barrel_idx)
+		color_idx = self.get_object_color_idx(self.attached_barrel_plate_idxs[barrel_idx])
+		if color_idx is None:
+			return None
+		return EXTENDED_COLOR_RGB_VALUES[color_idx]
+
+	def assign_object_colors(self):
+		plate_gate_idxs = [[] for _ in range(len(self.plate_cells))]
+		for gate_idx in range(len(self.gate_cells)):
+			plate_idxs = self.attached_gate_plate_idxs[gate_idx]
+			for plate_idx in plate_idxs:
+				plate_gate_idxs[plate_idx].append(gate_idx)
+
+		plate_barrel_idxs = [[] for _ in range(len(self.plate_cells))]
+		for barrel_idx in range(len(barrels)):
+			plate_idxs = self.attached_barrel_plate_idxs[barrel_idx]
+			for plate_idx in plate_idxs:
+				plate_barrel_idxs[plate_idx].append(barrel_idx)
+
+		plate_seen_color_idxs = {}
+		new_color_idx = 0
+		self.plate_color_idxs = []
+		for plate_idx in range(len(self.plate_cells)):
+			plate_seen_key = tuple(plate_gate_idxs[plate_idx]) + (None,) + tuple(plate_barrel_idxs[plate_idx])
+			if plate_seen_key == (None,):
+				color_idx = None
+			elif plate_seen_key in plate_seen_color_idxs:
+				color_idx = plate_seen_color_idxs[plate_seen_key]
+			else:
+				color_idx = new_color_idx
+				new_color_idx += 1
+				plate_seen_color_idxs[plate_seen_key] = color_idx
+			self.plate_color_idxs.append(color_idx)
+
+	def on_set_theme(self):
+		def make_grayscale_image(image):
+			gray_image = pygame.transform.grayscale(image)
+			gray_image.fill((60, 60, 60), special_flags=pygame.BLEND_RGB_ADD)
+			return gray_image
+		def colorize_image(image, color):
+			image.fill(color, special_flags=pygame.BLEND_RGB_MULT)
+			return image
+
+		self.color_floor_images = []
+		gray_floor_image = make_grayscale_image(self.Globals.load_theme_cell_image('floor'))
+		for color_idx in range(len(self.plate_color_idxs)):
+			color = EXTENDED_COLOR_RGB_VALUES[color_idx % len(EXTENDED_COLOR_RGB_VALUES)]
+			self.color_floor_images.append(colorize_image(gray_floor_image.copy(), color))
+
+		for barrel in barrels:
+			color = self.get_barrel_color(barrel)
+			if color is None:
+				continue
+			barrel.image = colorize_image(make_grayscale_image(barrel._orig_surf), color)
 
 	def store_level(self, stored_level):
 		stored_level["plate_cells"] = self.plate_cells
@@ -24,6 +108,20 @@ class SwitchBoxPuzzle(Puzzle):
 		self.gate_cells = stored_level["gate_cells"]
 		self.attached_gate_plate_idxs = stored_level["attached_gate_plate_idxs"]
 		self.attached_barrel_plate_idxs = stored_level["attached_barrel_plate_idxs"]
+		self.assign_object_colors()
+
+	def get_cell_image_to_draw(self, cell, cell_type):
+		if self.hide_colors:
+			return None
+		if cell_type == CELL_FLOOR and self.map[cell] == CELL_PLATE:
+			return self.get_plate_color_image(cell)
+		if cell_type == CELL_FLOOR and self.map[cell] in (CELL_GATE0, CELL_GATE1):
+			return self.get_gate_color_image(cell)
+		return None
+
+	def on_press_key(self, keyboard):
+		if keyboard.c:
+			self.hide_colors = not self.hide_colors
 
 	def is_plate_pressed(self, plate_idx):
 		plate_cell = self.plate_cells[plate_idx]
@@ -46,6 +144,9 @@ class SwitchBoxPuzzle(Puzzle):
 			be_phased = self.is_object_triggered(barrel.c == char.c, self.attached_barrel_plate_idxs[barrel_idx])
 			if be_phased != barrel.phased:
 				self.Globals.toggle_actor_phased(barrel)
+
+	def on_enter_room(self):
+		set_status_message("Press 'c' to hide or show colors", self, 0, 12)
 
 	def on_load_map(self, special_cell_values, extra_values):
 		plate_cells = self.get_map_cells(CELL_PLATE)
@@ -80,6 +181,8 @@ class SwitchBoxPuzzle(Puzzle):
 		for barrel_idx in range(len(barrels)):
 			plate_idxs = [ plate_idx for plate_idx, barrel_idxs in enumerate(attached_plate_barrel_idxs) if barrel_idx in barrel_idxs ]
 			self.attached_barrel_plate_idxs.append(plate_idxs)
+
+		self.assign_object_colors()
 
 	def on_prepare_enter_cell(self):
 		self.update_gate_and_barrel_states()
