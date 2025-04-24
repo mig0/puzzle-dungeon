mig0/puzzle-dungeon

commit 1700b9c71b34d33a82c350ecf3040f135e3718f2

Puzzle: Introduce is_char_phased and use in puzzles
Add new puzzle callback that returns False by default:
	puzzle.is_char_phased()

It is currently called on enter_room (after on_enter_room) and in
prepare_enter_cell (after on_prepare_enter_cell).

Use this in Atomix, Fifteen, Memory and Rotatepic puzzles.

Now there is no need for these puzzles to listen to update() that is
several times per second. They just implement this boolean callback that
is called when char starts or is moved. And now there is no need to reset
the phased state on finish(). The char state is automatically reset on
every new level.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/atomix.py
@@ -467,9 +467,6 @@ class AtomixPuzzle(Puzzle):
 	def on_press_key(self, keyboard):
 		self.draw_solved_mode = keyboard.kp_enter and not self.draw_solved_mode
 
-	def set_char_opacity_if_needed(self):
-		char.set_default_opacity(MEMORY_PUZZLE_CHAR_OPACITY if get_actor_on_cell(char.c, lifts) else 1)
-
 	def on_update(self, level_time):
 		self.set_char_opacity_if_needed()
 
@@ -480,6 +477,6 @@ class AtomixPuzzle(Puzzle):
 					lift.cell_to_draw = lift.goal_cell if self.draw_solved_mode else None
 				self.last_draw_solved_mode = self.draw_solved_mode
 
-	def finish(self):
-		char.set_default_opacity(1)
+	def is_char_phased(self):
+		return is_cell_in_actors(char.c, lifts)
 

main.py
@@ -489,6 +489,7 @@ def enter_room(idx):
 	game.start_level(map)
 
 	puzzle.on_enter_room()
+	char.phased = puzzle.is_char_phased()
 
 def get_max_area_distance(area):
 	return cell_distance((area.x1, area.y1), (area.x2, area.y2))
@@ -1865,6 +1866,7 @@ def prepare_enter_cell(animate_duration):
 		drop_key2.consume()
 
 	puzzle.on_prepare_enter_cell()
+	char.phased = puzzle.is_char_phased()
 
 def enter_cell():
 	# collect drop if any

puzzle/__init__.py
@@ -246,6 +246,9 @@ class Puzzle:
 	def finish(self):
 		pass
 
+	def is_char_phased(self):
+		return False
+
 	def prepare_solution(self):
 		return None
 

puzzle/fifteen.py
@@ -142,12 +142,6 @@ class FifteenPuzzle(Puzzle):
 	def on_press_key(self, keyboard):
 		self.draw_solved_mode = keyboard.kp_enter and not self.draw_solved_mode
 
-	def set_char_opacity_if_needed(self):
-		char.set_default_opacity(MEMORY_PUZZLE_CHAR_OPACITY if self.fifteen_map[char.c] != FIFTEEN_PUZZLE_VALUE_OUTSIDE else 1)
-
-	def on_update(self, level_time):
-		self.set_char_opacity_if_needed()
-
-	def finish(self):
-		char.set_default_opacity(1)
+	def is_char_phased(self):
+		return self.fifteen_map[char.c] != FIFTEEN_PUZZLE_VALUE_OUTSIDE
 

puzzle/memory.py
@@ -171,14 +171,8 @@ class MemoryPuzzle(Puzzle):
 		if keyboard.kp_enter:
 			self.reveal_time = self.level_time + 5
 
-	def set_char_opacity_if_needed(self):
-		if not self.use_colors:
-			char.set_default_opacity(MEMORY_PUZZLE_CHAR_OPACITY if self.memory_map[char.c] != MEMORY_PUZZLE_VALUE_OUTSIDE else 1)
-
 	def on_update(self, level_time):
 		self.level_time = level_time
-
-		self.set_char_opacity_if_needed()
 
 		if self.open_cell2:
 			if self.open_cell2_time:
@@ -187,6 +181,6 @@ class MemoryPuzzle(Puzzle):
 			else:
 				self.open_cell2_time = level_time + MEMORY_PUZZLE_OPEN_CELL2_TIME
 
-	def finish(self):
-		char.set_default_opacity(1)
+	def is_char_phased(self):
+		return not self.use_colors and self.memory_map[char.c] != MEMORY_PUZZLE_VALUE_OUTSIDE
 

puzzle/rotatepic.py
@@ -94,14 +94,8 @@ class RotatepicPuzzle(Puzzle):
 			self.scramble()
 		self.draw_solved_mode = keyboard.kp_enter and not self.draw_solved_mode and not solution.is_active()
 
-	def set_char_opacity_if_needed(self):
-		char.set_default_opacity(MEMORY_PUZZLE_CHAR_OPACITY if self.rotatepic_map[char.c] != ROTATEPIC_PUZZLE_VALUE_OUTSIDE else 1)
-
-	def on_update(self, level_time):
-		self.set_char_opacity_if_needed()
-
-	def finish(self):
-		char.set_default_opacity(1)
+	def is_char_phased(self):
+		return self.rotatepic_map[char.c] != ROTATEPIC_PUZZLE_VALUE_OUTSIDE
 
 	def find_solution_func(self):
 		solution_items = []

commit 05c55b93ae954d9c400911460ce9b41c8de91a7c

AtomixPuzzle: Fix use of removed method
Fixes: 1700b9c71b34 ("Puzzle: Introduce is_char_phased and use in puzzles")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/atomix.py
@@ -468,8 +468,6 @@ class AtomixPuzzle(Puzzle):
 		self.draw_solved_mode = keyboard.kp_enter and not self.draw_solved_mode
 
 	def on_update(self, level_time):
-		self.set_char_opacity_if_needed()
-
 		if not active_inplace_animation_actors:
 			if self.draw_solved_mode != self.last_draw_solved_mode:
 				room_lifts = self.Globals.get_actors_in_room(lifts)