mig0/dungeon

commit 4f8da3fbbd6ae1654a6c87270d4ad5e7a007e9b4

Refactor puzzle.generate_room call
Now there are 2 calls, one optional
puzzle.set_finish_cell(accessible_cells, finish_cell) and one
puzzle.generate_room() without arguments, to optimize to the common case.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/stoneage.py
@@ -64,6 +64,6 @@ class StoneagePuzzle(Puzzle):
 		drop_key1.instantiate(self.Globals.get_random_floor_cell())
 		drop_key2.instantiate(self.Globals.get_random_floor_cell())
 
-	def generate_room(self, accessible_cells, finish_cell):
+	def generate_room(self):
 		self.generate_random_solvable_room()
 

main.py
@@ -651,8 +651,9 @@ def generate_room(idx):
 		accessible_cells.pop(0)  # remove char cell
 		finish_cell = accessible_cells.pop()
 		map[finish_cell] = CELL_FINISH
+		puzzle.set_finish_cell(accessible_cells, finish_cell)
 
-	puzzle.generate_room(accessible_cells, finish_cell)
+	puzzle.generate_room()
 
 	# generate enemies
 	for i in range(level["num_enemies"] if "num_enemies" in level else DEFAULT_NUM_ENEMIES):

puzzle/__init__.py
@@ -11,6 +11,8 @@ class Puzzle:
 		self.room = None
 		self.level = level
 		self.Globals = Globals
+		self.accessible_cells = None
+		self.finish_cell = None
 		self.init()
 
 	def init(self):
@@ -58,7 +60,11 @@ class Puzzle:
 	def on_set_room(self, room):
 		self.room = room
 
-	def generate_room(self, accessible_cells, finish_cell):
+	def set_finish_cell(self, accessible_cells, finish_cell):
+		self.accessible_cells = accessible_cells
+		self.finish_cell = finish_cell
+
+	def generate_room(self):
 		pass
 
 	def on_generate_map(self):

puzzle/barrel.py
@@ -151,7 +151,7 @@ class BarrelPuzzle(Puzzle):
 			else:
 				break
 
-	def generate_room(self, accessible_cells, finish_cell):
+	def generate_room(self):
 		self.generate_random_solvable_room()
 
 	def is_solved(self):

puzzle/color.py
@@ -73,7 +73,7 @@ class ColorPuzzle(Puzzle):
 			return self.get_cell_image(cell)
 		return None
 
-	def generate_room(self, accessible_cells, finish_cell):
+	def generate_room(self):
 		for cell in product(self.area.x_range, self.area.y_range):
 			self.color_map[cell] = COLOR_PUZZLE_VALUE_GREEN
 			if self.is_plate(cell):

puzzle/fifteen.py
@@ -116,7 +116,7 @@ class FifteenPuzzle(Puzzle):
 			empty_cell = cell
 			num_moves_left -= 1
 
-	def generate_room(self, accessible_cells, finish_cell):
+	def generate_room(self):
 		# create the solved position - populate numbers from 1 to max_num
 		real_fifteen_map = arange(self.max_num).reshape(self.area.size_y, self.area.size_x)
 		real_fifteen_map += 1
@@ -125,7 +125,7 @@ class FifteenPuzzle(Puzzle):
 		self.scramble()
 
 		if self.is_solved():
-			self.generate_room(accessible_cells, finish_cell)
+			self.generate_room()
 
 	def modify_cell_types_to_draw(self, cell, cell_types):
 		if self.fifteen_map[cell] == FIFTEEN_PUZZLE_VALUE_OUTSIDE:

puzzle/gate.py
@@ -163,8 +163,8 @@ class GatePuzzle(Puzzle):
 			print("Can't generate gate puzzle, sorry")
 			quit()
 
-	def generate_room(self, accessible_cells, finish_cell):
-		self.generate_random_solvable_room(accessible_cells, finish_cell)
+	def generate_room():
+		self.generate_random_solvable_room(self.accessible_cells, self.finish_cell)
 
 	def on_press_key(self, keyboard):
 		if keyboard.space and self.map[char.c] == CELL_PLATE:

puzzle/lock.py
@@ -67,6 +67,6 @@ class LockPuzzle(Puzzle):
 			print("Can't generate lock puzzle, sorry")
 			quit()
 
-	def generate_room(self, accessible_cells, finish_cell):
-		self.generate_random_solvable_room(accessible_cells, finish_cell)
+	def generate_room(self):
+		self.generate_random_solvable_room(self.accessible_cells, self.finish_cell)
 

mig0/puzzle-dungeon

commit 1e29eeb5f103e0eeda1752caa48b769bfe7a965d

StoneAgePuzzle: Remove duplicate old method
Fixes: 4f8da3fbbd6a ("Refactor puzzle.generate_room call")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/stoneage.py
@@ -22,9 +22,6 @@ class StoneagePuzzle(Puzzle):
 	def has_sand(self):
 		return True
 
-	def generate_room(self, room, accessible_cells, finish_cell):
-		generate_random_solvable_stoneage_room()
-
 	def generate_random_nonsolvable_room(self):
 		self.Globals.replace_random_floor_cell(CELL_VOID, 70)
 		self.Globals.replace_random_floor_cell(CELL_START, 1, self.Globals.set_char_cell)