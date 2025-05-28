29af59558605f1be46a5d92e70b0cdb551a406e6
Mikhael Goikhman <migo@freeshell.org>
2025-05-28 16:25:50 +0300

    In translation func _() support sub-translations & handle bidi correctly
    Support interpolation of translated sub-strings, for example:
    	"{level-goal} {in-word} 50 {seconds-word}"
    may be translated to English like:
    	Reach finish in 50 seconds
    or to French like:
    	Atteindre l'arrivée en 50 secondes
    
    Now bidi for Hebrew is implemented pretty much correctly for most of
    cases without requiring external bidi module.
    
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import io
+import re
 import random
 import pygame
 import pgzero
@@ -9,6 +10,7 @@ from numpy import ndarray, chararray, array, any
 from copy import deepcopy
 from random import randint, choice, shuffle
 from traceback import extract_stack
+from unicodedata import bidirectional
 from constants import *
 from leveltools import *
 from translations import *
@@ -49,11 +51,49 @@ def autodetect_lang():
 	except:
 		pass
 
-def _(str_key):
-	str = translations[lang][str_key] if str_key in translations[lang] else translations['en'][str_key] if str_key in translations['en'] else str_key
-	if lang == 'he' and str_key in translations[lang]:
-		str = str[::-1]
-	return str
+# translate key like "default-goal" or embedded keys like "{reach-finish} {in-word} 5 {seconds-word}"
+def _(str_key, disable_bidi=False):
+	if str_key == '' or str_key is None:
+		return ''
+
+	def is_rtl(word):
+		for ch in word:
+			b = bidirectional(ch)
+			if b in ('R', 'AL'):
+				return True
+			if b in ('L', 'EN', 'AN'):
+				return False
+		return False  # default to LTR if no strong character
+
+	def bidi(str):
+		"""BiDi simulation using word-level RTL detection, preserving original whitespace separators."""
+		nonlocal disable_bidi
+		if disable_bidi or not str or lang != 'he' or not any(bidirectional(ch) in ('R', 'AL') for ch in str):
+			return str
+
+		parts = re.findall(r'\S+|\s+', str)
+		tokens = []
+
+		for part in parts:
+			if part.isspace():
+				tokens.append((part, False))  # separator
+			else:
+				tokens.append((part, is_rtl(part)))
+
+		if is_rtl(str):
+			tokens = reversed(tokens)
+
+		return ''.join(
+			word[::-1] if is_rtl else word
+			for word, is_rtl in tokens
+		)
+
+	# replace all {sub-key} occurrences (flat, no nesting)
+	str_key = re.sub(r'{([^{}]+)}', lambda m: _(m.group(1), True), str_key)
+
+	str = translations[lang].get(str_key) or translations['en'].get(str_key) or str_key
+
+	return bidi(str)
 
 autodetect_lang()
 
