mig0/puzzle-dungeon

commit 0549927720c2977e749d92cfea1ee611e3925a6c

Implement new mirror concept
Mirror is an object that reflects a beam (will soon be used in Mirror
Puzzle). It has orientation (one of four) and activeness (active or not,
plus flipped activeness or not). Inactive mirrors bypass a beam. Mirrors
with flipped activeness change their reflectiveness property depending on
whether it is the first or repeating encounter of the mirror by the beam.

Mirror is a property of cellactor that is None for regular actors and
contains an object of type Mirror inheriting from CellActor for mirror
hosts (carrier).

Currently the mirror carriers may be barrel or lift (and cart in near
future). Mirrors have separate image, but should be synced with their
carriers (position, movement, animation).

Update map chars accordingly (for regular actors and hosting a mirror).

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

constants.py
@@ -42,6 +42,7 @@ ACTOR_CHARS = {
 	'key2':   '²',
 	'enemy':  '🕱',
 	'barrel': '■',
+	'mirror': '▬',
 	'char':   '☻',
 	'npc':    '☀',
 }
@@ -54,6 +55,7 @@ ACTOR_ON_PLATE_CHARS = {
 	'key2':   '₂',
 	'enemy':  '☠',
 	'barrel': '□',
+	'mirror': '▭',
 	'char':   '☺',
 	'npc':    '☼',
 }
@@ -100,18 +102,35 @@ MOVE_TYPE_DIRS = {
 }
 MOVE_TYPES = *MOVE_TYPE_DIRS,
 
-LIFT_CHARS = {
-	MOVE_A: '✥',
-	MOVE_H: '↔',
-	MOVE_V: '↕',
-	MOVE_L: '←',
-	MOVE_R: '→',
-	MOVE_U: '↑',
-	MOVE_D: '↓',
-	MOVE_N: '◉',
-}
+LIFT_CHARS = [
+	# regular
+	{
+		MOVE_A: '✥',
+		MOVE_H: '↔',
+		MOVE_V: '↕',
+		MOVE_L: '←',
+		MOVE_R: '→',
+		MOVE_U: '↑',
+		MOVE_D: '↓',
+		MOVE_N: '◉',
+	},
+	# mirror
+	{
+		MOVE_A: '✤',
+		MOVE_H: '⇆',
+		MOVE_V: '⇅',
+		MOVE_L: '⇇',
+		MOVE_R: '⇉',
+		MOVE_U: '⇈',
+		MOVE_D: '⇊',
+		MOVE_N: '◎',
+	},
+]
 
-LIFT_MOVE_TYPES_BY_CHAR = {v: k for k, v in LIFT_CHARS.items()}
+LIFT_MOVE_TYPES_BY_CHAR = {v: k for d in LIFT_CHARS for k, v in d.items()}
+
+MIRROR_CHARS = tuple(v for d in (LIFT_CHARS[1],) for k, v in d.items())
+MIRROR_ORIENTATION_CHARS = [ '-', '/', '|', '\\' ]
 
 IMAGES_DIR_PREFIX = DATA_DIR + '/images/'
 DEFAULT_IMAGE_PREFIX = 'default/'

cellactor.py
@@ -102,6 +102,7 @@ class CellActor(Actor):
 		self.cell_to_draw = None
 		self._deferred_transform = False
 		self._pending_transform = False
+		self._mirror = None
 
 		self.reset_state()
 		self.animation = None
@@ -115,6 +116,8 @@ class CellActor(Actor):
 
 	def draw(self, cell=None, opacity=None):
 		if self.hidden:
+			if self.mirror:
+				self.mirror.draw(cell, opacity)
 			return
 		if self.phased and opacity is None and not self._inplace_animation_active:
 			opacity = ACTOR_PHASED_OPACITY
@@ -127,6 +130,8 @@ class CellActor(Actor):
 			real_opacity = self.opacity
 			self.opacity = opacity
 		super().draw()
+		if self.mirror:
+			self.mirror.draw(cell, opacity)
 		if cell:
 			self.c = real_cell
 		if opacity is not None:
@@ -156,6 +161,8 @@ class CellActor(Actor):
 	def c(self, cell):
 		self._cell = NONE_CELL if cell is None else cell
 		self.x, self.y = self.pos = self.get_pos()
+		if self.mirror:
+			self.mirror.c = self.c
 
 	@property
 	def s(self):
@@ -258,6 +265,16 @@ class CellActor(Actor):
 			print("CellActor.image: Unsupported type: " + str(image))
 			pass
 
+	@property
+	def mirror(self):
+		return self._mirror
+
+	@mirror.setter
+	def mirror(self, mirror):
+		self._mirror = mirror
+		if mirror is not None:
+			mirror.c = self.c
+
 	def reset_state(self):
 		self._cell, self._size, self.hidden, self.phased = NONE_CELL, None, False, False
 
@@ -298,10 +315,13 @@ class CellActor(Actor):
 		old_cell = self.c
 		self.move(diff)
 		if enable_animation:
-			self.pos = old_pos
 			distance = cell_distance(old_cell, target)
 			animate_time_factor = distance - (distance - 1) / 2
+			self.pos = old_pos
 			self.animate(animate_time_factor * ARROW_KEYS_RESOLUTION, on_finished=on_finished)
+			if self.mirror:
+				self.mirror.pos = old_pos
+				self.mirror.animate(animate_time_factor * ARROW_KEYS_RESOLUTION)
 
 	def _transform(self):
 		if not hasattr(self, '_orig_surf'):
@@ -482,12 +502,15 @@ def create_actor(image_name, cell):
 	actor.c = cell
 	return actor
 
-def get_actor_on_cell(cell, actors, include_hidden=False, include_phased=False):
+def get_actor_on_cell(cell, actors, include_hidden=None, include_phased=None):
 	for actor in actors:
-		if (include_hidden or not actor.hidden) and (include_phased or not actor.phased) and cell == actor.c:
+		if (cell == actor.c
+			and (not actor.hidden or (actor.mirror if include_hidden is None else include_hidden))
+			and (not actor.phased or (actor.mirror if include_phased is None else include_phased))
+		):
 			return actor
 	return None
 
-def is_cell_in_actors(cell, actors, include_hidden=False, include_phased=False):
+def is_cell_in_actors(cell, actors, include_hidden=None, include_phased=None):
 	return get_actor_on_cell(cell, actors, include_hidden, include_phased) is not None
 

config.py
@@ -59,6 +59,8 @@ DROP_FACTOR_COLORS  = ("#3030FF", "#2020AA", "#FFFFFF")
 ACTOR_PHASED_OPACITY = 0.4
 ACTOR_PHASED_DURATION = 0.3
 
+MIRROR_INACTIVE_OPACITY = 0.4
+
 MEMORY_PUZZLE_OPEN_CELL2_TIME = 1
 MEMORY_PUZZLE_REVEAL_FADE_DURATION = 1.5
 MEMORY_PUZZLE_CHAR_OPACITY = 0.4

main.py
@@ -18,6 +18,7 @@ from translations import *
 from cellactor import *
 from objects import *
 from common import *
+from mirror import *
 from debug import *
 from image import *
 from theme import *
@@ -108,6 +109,7 @@ def load_map(filename_or_stringio, special_cell_types={}):
 			print(filename_or_stringio.getvalue())
 		enemies.clear()
 		barrels.clear()
+		lifts.clear()
 		set_char_cell(None, 0)
 
 	if is_stringio:
@@ -159,10 +161,14 @@ def load_map(filename_or_stringio, special_cell_types={}):
 				return
 			ch = line[x]
 			cell = (x, y)
+			mirror_host = None
+			value_type = None
 			if ch == CELL_START:
 				set_char_cell(cell, 0)
 			if ch in LIFT_MOVE_TYPES_BY_CHAR:
-				create_lift(cell, LIFT_MOVE_TYPES_BY_CHAR[ch])
+				lift = create_lift(cell, LIFT_MOVE_TYPES_BY_CHAR[ch])
+				if ch in MIRROR_CHARS:
+					mirror_host = lift
 				ch = CELL_VOID
 			if ch in ACTOR_AND_PLATE_BY_CHAR:
 				actor_name, is_plate = ACTOR_AND_PLATE_BY_CHAR[ch]
@@ -175,11 +181,17 @@ def load_map(filename_or_stringio, special_cell_types={}):
 					create_enemy(cell)
 				if actor_name == "barrel":
 					create_barrel(cell)
+				if actor_name == "mirror":
+					mirror_host = create_barrel(cell)
 				if actor_name == "char":
 					set_char_cell(cell, 0)
 				if actor_name == "npc":
 					special_cell_infos.append((cell, None))
-			if value_type := special_cell_types.get(ch):
+			if mirror_host:
+				mirror_host.mirror = Mirror(get_theme_image_name('mirror'), mirror_host)
+				value_type = 'strs'
+			value_type = value_type or special_cell_types.get(ch)
+			if value_type:
 				special_cell_infos.append((cell, value_type))
 			map[x, y] = ch
 		line_n += 1
@@ -192,7 +204,7 @@ def load_map(filename_or_stringio, special_cell_types={}):
 		line = file.readline()
 		if line == '':
 			map = orig_map.copy()
-			print_error("Failed to read value for special map cell %s" % str(cell))
+			print_error("Failed to read line for special map cell %s" % str(cell))
 			return
 		str = line.rstrip("\n")
 		def parse_int(str):
@@ -200,6 +212,8 @@ def load_map(filename_or_stringio, special_cell_types={}):
 		try:
 			if value_type == 'str':
 				value = str
+			elif value_type == 'strs':
+				value = str.split()
 			elif value_type == 'int':
 				value = parse_int(str)
 			elif value_type == 'ints':
@@ -332,10 +346,10 @@ def debug_map(level=0, descr=None, full_format=False, full=True, clean=True, com
 					cell_ch = actor_chars[drop.name]
 				if is_cell_in_actors(cell, enemies):
 					cell_ch = actor_chars['enemy']
-				if is_cell_in_actors(cell, barrels):
-					cell_ch = actor_chars['barrel']
+				if barrel := get_actor_on_cell(cell, barrels):
+					cell_ch = actor_chars['mirror' if barrel.mirror else 'barrel']
 				if lift := get_actor_on_cell(cell, lifts):
-					cell_ch = LIFT_CHARS[lift.type]
+					cell_ch = LIFT_CHARS[1 if lift.mirror else 0][lift.type]
 				if cell == char_cell or char.c is not None and char.c == cell:
 					cell_ch = actor_chars['char']
 				print(cell_ch, end="")

mirror.py
@@ -1,0 +1,77 @@
+import pygame
+from typing import Union
+from cellactor import *
+from objects import *
+from common import die
+from game import game
+from constants import MIRROR_INACTIVE_OPACITY, MIRROR_ORIENTATION_CHARS
+
+class Mirror(CellActor):
+	def __init__(self, image:Union[str, pygame.Surface]=None, host:CellActor=None, orientation=0, activeness:int=0):
+		super().__init__(image)
+		self.host = host
+		self.orientation = orientation
+		self.activeness = activeness
+		self.fixed_orientation = False
+		self.fixed_activeness = False
+
+	def draw(self, cell=None, opacity=None):
+		self.sync_rotation()
+		if not self.is_active():
+			opacity = MIRROR_INACTIVE_OPACITY * (self.opacity if opacity is None else opacity)
+		super().draw(cell, opacity)
+		if self.is_active_flip():
+			pos = apply_diff(self.pos, (20, -20))
+			tip = 'x'
+			game.screen.draw.text(tip, center=pos, color="#60C0FF", gcolor="#0080A0", owidth=1.2, ocolor="#404030", alpha=1, fontsize=16)
+
+	@property
+	def host(self):
+		return self._host
+
+	@host.setter
+	def host(self, host:CellActor):
+		self._host = host
+		host.mirror = self
+
+	@property
+	def orientation(self):
+		return self._orientation
+
+	@orientation.setter
+	def orientation(self, orientation):
+		if type(orientation) == str and orientation in MIRROR_ORIENTATION_CHARS:
+			orientation = MIRROR_ORIENTATION_CHARS.index(orientation)
+		if not (type(orientation) == int and 0 <= orientation <= 3):
+			die("Invalid mirror orientation (%s), fix the bug" % str(orientation))
+		self._orientation = orientation
+
+	def reset(self):
+		if not self.fixed_orientation:
+			self.orientation = 0
+		if not self.fixed_activeness:
+			self.activeness = 0
+
+	def sync_rotation(self):
+		self.angle = self.orientation * (-45)
+
+	def rotate_mirror(self, incr):
+		self.orientation = (self.orientation + incr) % 4
+
+	def is_active(self, visited=False):
+		return (self.activeness % 2 == 1) ^ (visited and self.is_active_flip())
+
+	def toggle_active(self):
+		self.activeness ^= 1
+
+	def is_active_flip(self):
+		return self.activeness // 2 == 1
+
+	def toggle_active_flip(self):
+		self.activeness ^= 2
+
+def get_mirror_on_cell(cell):
+	for actors in (barrels, lifts):
+		if (actor := get_actor_on_cell(cell, actors)) and actor.mirror:
+			return actor.mirror
+	return None

tools/convert-mosaic-map
@@ -46,13 +46,13 @@ mosaic_to_dungeon_chars = {
 	',': CELL_CRACK,
 	':': CELL_BONES,
 	';': CELL_ROCKS,
-	'a': LIFT_CHARS[MOVE_A],
-	'h': LIFT_CHARS[MOVE_H],
-	'v': LIFT_CHARS[MOVE_V],
-	'l': LIFT_CHARS[MOVE_L],
-	'r': LIFT_CHARS[MOVE_R],
-	'u': LIFT_CHARS[MOVE_U],
-	'd': LIFT_CHARS[MOVE_D],
+	'a': LIFT_CHARS[0][MOVE_A],
+	'h': LIFT_CHARS[0][MOVE_H],
+	'v': LIFT_CHARS[0][MOVE_V],
+	'l': LIFT_CHARS[0][MOVE_L],
+	'r': LIFT_CHARS[0][MOVE_R],
+	'u': LIFT_CHARS[0][MOVE_U],
+	'd': LIFT_CHARS[0][MOVE_D],
 }
 
 dungeon_to_mosaic_chars = {v: k for k, v in mosaic_to_dungeon_chars.items()}

commit eb72e1ee04ce0d163eaeace54ee8eb959ea8b55d

Fix mirror chars used for orientation
This affects mostly load_map.

Fixes: 0549927720c2 ("Implement new mirror concept")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

constants.py
@@ -130,7 +130,7 @@ LIFT_CHARS = [
 LIFT_MOVE_TYPES_BY_CHAR = {v: k for d in LIFT_CHARS for k, v in d.items()}
 
 MIRROR_CHARS = tuple(v for d in (LIFT_CHARS[1],) for k, v in d.items())
-MIRROR_ORIENTATION_CHARS = [ '-', '/', '|', '\\' ]
+MIRROR_ORIENTATION_CHARS = [ '|', '/', '-', '\\' ]
 
 IMAGES_DIR_PREFIX = DATA_DIR + '/images/'
 DEFAULT_IMAGE_PREFIX = 'default/'