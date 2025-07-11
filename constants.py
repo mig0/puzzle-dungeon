mig0/puzzle-dungeon

commit bba4fd49cccb932d95d0eeac54137801333b4759

Add initial support for carts
Carts are actors similar to lifts with the same 8 movement types, but
move on floor, one cell per player instruction, in 0, 1, 2, or 4
directions depending on movement type.

They will be used in new Mirror Puzzle and Traffic Puzzle.

Additional support for carts will be added later.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

constants.py
@@ -102,6 +102,31 @@ MOVE_TYPE_DIRS = {
 }
 MOVE_TYPES = *MOVE_TYPE_DIRS,
 
+CART_CHARS = [
+	# regular
+	{
+		MOVE_A: '✣',
+		MOVE_H: '⇿',
+		MOVE_V: '⇳',
+		MOVE_L: '⇦',
+		MOVE_R: '⇧',
+		MOVE_U: '⇧',
+		MOVE_D: '⇩',
+		MOVE_N: '◍',
+	},
+	# mirror
+	{
+		MOVE_A: '✢',
+		MOVE_H: '⇔',
+		MOVE_V: '⇕',
+		MOVE_L: '⇐',
+		MOVE_R: '⇒',
+		MOVE_U: '⇓',
+		MOVE_D: '⇑',
+		MOVE_N: '◌',
+	},
+]
+
 LIFT_CHARS = [
 	# regular
 	{
@@ -127,9 +152,11 @@ LIFT_CHARS = [
 	},
 ]
 
+CART_MOVE_TYPES_BY_CHAR = {v: k for d in CART_CHARS for k, v in d.items()}
 LIFT_MOVE_TYPES_BY_CHAR = {v: k for d in LIFT_CHARS for k, v in d.items()}
+MOVE_TYPES_BY_CHAR = {**CART_MOVE_TYPES_BY_CHAR, **LIFT_MOVE_TYPES_BY_CHAR}
 
-MIRROR_CHARS = tuple(v for d in (LIFT_CHARS[1],) for k, v in d.items())
+MIRROR_CHARS = tuple(v for d in (CART_CHARS[1], LIFT_CHARS[1]) for k, v in d.items())
 MIRROR_ORIENTATION_CHARS = [ '|', '/', '-', '\\' ]
 
 IMAGES_DIR_PREFIX = DATA_DIR + '/images/'

main.py
@@ -109,6 +109,7 @@ def load_map(filename_or_stringio, special_cell_types={}):
 			print(filename_or_stringio.getvalue())
 		enemies.clear()
 		barrels.clear()
+		carts.clear()
 		lifts.clear()
 		portal_destinations.clear()
 		set_char_cell(None, 0)
@@ -167,6 +168,11 @@ def load_map(filename_or_stringio, special_cell_types={}):
 			value_type = None
 			if ch == CELL_START:
 				set_char_cell(cell, 0)
+			if ch in CART_MOVE_TYPES_BY_CHAR:
+				cart = create_cart(cell, CART_MOVE_TYPES_BY_CHAR[ch])
+				if ch in MIRROR_CHARS:
+					mirror_host = cart
+				ch = CELL_FLOOR
 			if ch in LIFT_MOVE_TYPES_BY_CHAR:
 				lift = create_lift(cell, LIFT_MOVE_TYPES_BY_CHAR[ch])
 				if ch in MIRROR_CHARS:
@@ -384,6 +390,8 @@ def debug_map(level=0, descr=None, full_format=False, full=True, clean=True, com
 					cell_ch = actor_chars['enemy']
 				if barrel := get_actor_on_cell(cell, barrels):
 					cell_ch = actor_chars['mirror' if barrel.mirror else 'barrel']
+				if cart := get_actor_on_cell(cell, carts):
+					cell_ch = CART_CHARS[1 if cart.mirror else 0][cart.type]
 				if lift := get_actor_on_cell(cell, lifts):
 					cell_ch = LIFT_CHARS[1 if lift.mirror else 0][lift.type]
 				if cell == char_cell or char.c is not None and char.c == cell:
@@ -1132,6 +1140,9 @@ def set_theme(theme_name):
 	for barrel in barrels:
 		reload_actor_theme_image(barrel)
 
+	for cart in carts:
+		reload_actor_theme_image(cart)
+
 	for lift in lifts:
 		reload_actor_theme_image(lift)
 
@@ -1234,7 +1245,7 @@ def init_new_level(offset=1, config=None, reload_stored=False):
 	global revealed_map
 	global switch_cell_infos, portal_demolition_infos
 	global char_cells, enter_room_idx
-	global enemies, barrels, killed_enemies, lifts
+	global enemies, barrels, killed_enemies, carts, lifts
 	global level_time
 	global map, stored_level
 
@@ -1274,6 +1285,7 @@ def init_new_level(offset=1, config=None, reload_stored=False):
 
 	barrels.clear()
 	enemies.clear()
+	carts.clear()
 	lifts.clear()
 	killed_enemies.clear()
 	portal_destinations.clear()
@@ -1511,6 +1523,8 @@ def draw():
 
 	draw_map()
 	draw_status()
+	for cart in carts:
+		cart.draw()
 	for lift in lifts:
 		lift.draw()
 	for drop in drops:

mirror.py
@@ -71,7 +71,7 @@ class Mirror(CellActor):
 		self.activeness ^= 2
 
 def get_mirror_on_cell(cell):
-	for actors in (barrels, lifts):
+	for actors in (barrels, carts, lifts):
 		if (actor := get_actor_on_cell(cell, actors)) and actor.mirror:
 			return actor.mirror
 	return None

objects.py
@@ -8,10 +8,10 @@ from drop import Drop
 
 __all__ = [
 	'set_map',  # temporarily
-	'char', 'enemies', 'barrels', 'lifts', 'portal_destinations',
+	'char', 'enemies', 'barrels', 'carts', 'lifts', 'portal_destinations',
 	'drop_heart', 'drop_sword', 'drop_might', 'drop_key1', 'drop_key2', 'drops',
 	'cursor', 'create_enemy', 'create_barrel',
-	'create_lift', 'get_lift_target', 'get_lift_target_at_neigh',
+	'create_cart', 'create_lift', 'get_lift_target', 'get_lift_target_at_neigh',
 	'create_portal', 'create_portal_pair',
 ]
 
@@ -26,6 +26,7 @@ char = Fighter()
 
 enemies = []
 barrels = []
+carts = []
 lifts = []
 
 portal_destinations = {}
@@ -70,6 +71,30 @@ def create_barrel(cell):
 	barrels.append(barrel)
 
 	return barrel
+
+def create_cart(cell, move_type, exit_cell=None, image_name=None, surface=None):
+	global carts
+
+	opacity = None
+	if image_name is None:
+		image_name = "lift" + move_type
+		opacity = 0.5
+	cart = create_actor(surface, cell) if surface else create_theme_actor(image_name, cell)
+	cart.type = move_type
+	if opacity is not None:
+		cart.opacity = opacity
+	if not surface:
+		angle = 0
+		if move_type in (MOVE_H, MOVE_L):
+			angle = -90
+		elif move_type in (MOVE_R):
+			angle = 90
+		elif move_type in (MOVE_D):
+			angle = 180
+		cart.angle = angle
+	carts.append(cart)
+
+	return cart
 
 def create_lift(cell, move_type, surface=None):
 	global lifts

commit 946c511d224e7835658e52ad1ca93dd68cf7c1ba

Fix 3 unicode values in CART_CHARS
These will be used soon, so make sure they are correct.

Fixes: bba4fd49cccb ("Add initial support for carts")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

constants.py
@@ -109,7 +109,7 @@ CART_CHARS = [
 		MOVE_H: '⇿',
 		MOVE_V: '⇳',
 		MOVE_L: '⇦',
-		MOVE_R: '⇧',
+		MOVE_R: '⇨',
 		MOVE_U: '⇧',
 		MOVE_D: '⇩',
 		MOVE_N: '◍',
@@ -121,8 +121,8 @@ CART_CHARS = [
 		MOVE_V: '⇕',
 		MOVE_L: '⇐',
 		MOVE_R: '⇒',
-		MOVE_U: '⇓',
-		MOVE_D: '⇑',
+		MOVE_U: '⇑',
+		MOVE_D: '⇓',
 		MOVE_N: '◌',
 	},
 ]