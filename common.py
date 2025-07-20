529464c717bc8c85ddec09861416e3d79e442282
Mikhael Goikhman <migo@freeshell.org>
2025-07-20 16:25:03 +0300

    common: Add function get_pgzero_game_from_stack
    This is mainly for hacks to inject missing pgzero behaviours.
    
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- common.py
+++ common.py
@@ -1,3 +1,4 @@
+import inspect
 from traceback import extract_stack
 
 def warn(error, with_trace=False):
@@ -11,4 +12,14 @@ def warn(error, with_trace=False):
 def die(error, with_trace=False):
 	warn(error, with_trace)
 	quit()
+
+def get_pgzero_game_from_stack():
+	for frame_info in inspect.stack():
+		frame = frame_info.frame
+		func_name = frame.f_code.co_name
+		if func_name == 'mainloop' and 'self' in frame.f_locals:
+			self_obj = frame.f_locals['self']
+			if type(self_obj).__name__ == 'PGZeroGame':
+					return self_obj
+	die("PGZeroGame not found in stack", True)
 
