mig0/puzzle-dungeon

commit fbfa8aaae42eb2a9d8b8ad616ce9efee6b8d26e2

Refactor global room and related functions into separate room.py
Remove Puzzle set_room hook, since room is global now. Puzzles may access
room instead of self.room. The puzzles still need on_set_room hook as a
notification when the room is changed. Also, the puzzles should not
import room related functions via main Globals mechanism. They may access
them directly.

Have set_room_and_notify_puzzle instead of set_room in main.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -19,6 +19,7 @@ from translations import *
 from cellactor import *
 from objects import *
 from debug import *
+from room import *
 from game import game
 from drop import draw_status_drops
 from flags import flags
@@ -231,9 +232,6 @@ def load_map(filename_or_stringio, special_cell_types={}):
 
 	return (special_cell_values, extra_values)
 
-def is_cell_in_area(cell, x_range, y_range):
-	return cell[0] in x_range and cell[1] in y_range
-
 # get 4 neughbour cells for cell
 def get_cell_neighbors(cell, x_range=None, y_range=None):
 	neighbors = []
@@ -311,7 +309,6 @@ level_goal_time = 0
 
 level = None
 
-room = Area()
 enter_room_idx = None
 
 def get_bg_image():
@@ -493,35 +490,21 @@ def get_revealed_actors(actors):
 def set_char_flip(is_right_dir=True):
 	char.flip = None if is_right_dir else (True, False)
 
-def assert_room():
-	if mode != "game" and mode != "init" and mode != "next":
-		die("Called room function when not inside game or init (mode=%s). Fix this bug" % mode)
-
-def set_room(idx):
-	room.size = flags.ROOM_SIZE(idx)
-	room.size_x = flags.ROOM_SIZE_X[idx]
-	room.size_y = flags.ROOM_SIZE_Y[idx]
-	room.x1 = flags.ROOM_X1[idx]
-	room.x2 = flags.ROOM_X2[idx]
-	room.y1 = flags.ROOM_Y1[idx]
-	room.y2 = flags.ROOM_Y2[idx]
-	room.x_range = flags.ROOM_X_RANGE[idx]
-	room.y_range = flags.ROOM_Y_RANGE[idx]
-	room.idx = idx
-
-	puzzle.set_room(room)
+def set_room_and_notify_puzzle(idx):
+	set_room(idx)
+	puzzle.on_set_room()
 
 # only to be used by puzzle's restore_level
 def advance_room():
 	if room.idx + 1 >= flags.NUM_ROOMS:
 		return False
-	set_room(room.idx + 1)
+	set_room_and_notify_puzzle(room.idx + 1)
 	return True
 
 def enter_room(idx):
 	global mode, char_cells, last_time_arrow_keys_processed
 
-	set_room(idx)
+	set_room_and_notify_puzzle(idx)
 	reset_status_messages()
 
 	char.reset_animation()
@@ -547,23 +530,6 @@ def enter_room(idx):
 
 	char.phased = puzzle.is_char_phased()
 	set_char_flip((char.cx - room.x1) * 2 < room.size_x)
-
-def get_max_area_distance(area):
-	return cell_distance((area.x1, area.y1), (area.x2, area.y2))
-
-def get_max_room_distance():
-	return get_max_area_distance(room)
-
-def is_actor_in_room(actor):
-	assert_room()
-
-	return actor.cx >= room.x1 and actor.cx <= room.x2 and actor.cy >= room.y1 and actor.cy <= room.y2
-
-def get_actors_in_room(actors):
-	return [actor for actor in actors if is_actor_in_room(actor)]
-
-def is_cell_in_room(cell):
-	return is_cell_in_area(cell, room.x_range, room.y_range)
 
 accessible_obstacles = None
 
@@ -1066,10 +1032,6 @@ class Globals:
 	create_text_cell_image = create_text_cell_image
 	is_cell_occupied = is_cell_occupied
 	advance_room = advance_room
-	get_max_room_distance = get_max_room_distance
-	is_actor_in_room = is_actor_in_room
-	is_cell_in_room = is_cell_in_room
-	get_actors_in_room = get_actors_in_room
 	start_accessible_obstacles = start_accessible_obstacles
 	clear_accessible_obstacles = clear_accessible_obstacles
 	is_cell_accessible = is_cell_accessible
@@ -1102,7 +1064,7 @@ class Globals:
 	toggle_actor_phased = toggle_actor_phased
 
 def generate_room(idx):
-	set_room(idx)
+	set_room_and_notify_puzzle(idx)
 
 	if flags.is_random_maze:
 		generate_random_maze_room()
@@ -1166,7 +1128,7 @@ def generate_map():
 			if flags.MULTI_ROOMS:
 				print("Ignoring multi-room level config when loading map")
 			puzzle.set_map(map)
-			set_room(0)
+			set_room_and_notify_puzzle(0)
 			puzzle.on_load_map(*ret)
 			return
 
@@ -1433,7 +1395,7 @@ def init_new_level(offset=1, config=None, reload_stored=False):
 			create_portal(portal_cell, dst_cell)
 		for drop in drops:
 			drop.restore_state(stored_level["drop_states"][drop.name])
-		set_room(0)
+		set_room_and_notify_puzzle(0)
 		puzzle.restore_level(stored_level)
 	else:
 		theme_name = level["theme"]

puzzle/__init__.py
@@ -2,6 +2,7 @@ from constants import *
 from cellactor import *
 from objects import *
 from debug import *
+from room import *
 from flags import flags
 from time import time
 from random import randint, random, shuffle, choice, choices
@@ -26,7 +27,6 @@ class Puzzle:
 
 	def __init__(self, level, Globals):
 		self.map = None
-		self.room = None
 		self.level = level
 		self.Globals = Globals
 		self.area = Area()
@@ -102,10 +102,10 @@ class Puzzle:
 		return [cell for cell in self.area.cells if self.map[cell] in cell_types]
 
 	def get_room_cells(self, *cell_types):
-		return [cell for cell in self.room.cells if self.map[cell] in cell_types]
+		return [cell for cell in room.cells if self.map[cell] in cell_types]
 
 	def set_area_from_config(self, min_size=None, default_size=None, request_odd_size=False, align_to_center=False):
-		max_size = flags.ROOM_SIZE(self.room.idx, request_odd_size)
+		max_size = flags.ROOM_SIZE(room.idx, request_odd_size)
 		if min_size is None:
 			min_size = (3, 3) if request_odd_size else (2, 2)
 
@@ -122,13 +122,13 @@ class Puzzle:
 		self.area.size = tuple(size)
 		self.area.size_x = size[0]
 		self.area.size_y = size[1]
-		self.area.x1 = self.room.x1 + (self.room.size_x - self.area.size_x) // 2 \
-			+ ((self.room.size_x - self.area.size_x) % 2 * ((self.room.idx + 1 if self.room.idx is not None else 0) % 2) if align_to_center and flags.NUM_ROOMS == 4 else 0) \
-			+ ((self.room.size_x - self.area.size_x) % 2 * ((self.room.idx + 1 if self.room.idx is not None else 0) % 3) if align_to_center and flags.NUM_ROOMS == 9 else 0)
+		self.area.x1 = room.x1 + (room.size_x - self.area.size_x) // 2 \
+			+ ((room.size_x - self.area.size_x) % 2 * ((room.idx + 1 if room.idx is not None else 0) % 2) if align_to_center and flags.NUM_ROOMS == 4 else 0) \
+			+ ((room.size_x - self.area.size_x) % 2 * ((room.idx + 1 if room.idx is not None else 0) % 3) if align_to_center and flags.NUM_ROOMS == 9 else 0)
 		self.area.x2 = self.area.x1 + self.area.size_x - 1
-		self.area.y1 = self.room.y1 + (self.room.size_y - self.area.size_y) // 2 \
-			+ ((self.room.size_y - self.area.size_y) % 2 * (1 - ((self.room.idx if self.room.idx is not None else 2) // 2) % 2) if align_to_center and flags.NUM_ROOMS == 4 else 0) \
-			+ ((self.room.size_y - self.area.size_y) % 2 * int(1.5 - ((self.room.idx if self.room.idx is not None else 3) // 3) % 3) if align_to_center and flags.NUM_ROOMS == 9 else 0)
+		self.area.y1 = room.y1 + (room.size_y - self.area.size_y) // 2 \
+			+ ((room.size_y - self.area.size_y) % 2 * (1 - ((room.idx if room.idx is not None else 2) // 2) % 2) if align_to_center and flags.NUM_ROOMS == 4 else 0) \
+			+ ((room.size_y - self.area.size_y) % 2 * int(1.5 - ((room.idx if room.idx is not None else 3) // 3) % 3) if align_to_center and flags.NUM_ROOMS == 9 else 0)
 		self.area.y2 = self.area.y1 + self.area.size_y - 1
 		self.area.x_range = range(self.area.x1, self.area.x2 + 1)
 		self.area.y_range = range(self.area.y1, self.area.y2 + 1)
@@ -137,7 +137,7 @@ class Puzzle:
 		return self.Globals.is_cell_in_area(cell, self.area.x_range, self.area.y_range)
 
 	def is_in_room(self, cell):
-		return self.Globals.is_cell_in_area(cell, self.room.x_range, self.room.y_range)
+		return self.Globals.is_cell_in_area(cell, room.x_range, room.y_range)
 
 	def set_area_border_walls(self):
 		for x in self.area.x_range:
@@ -212,10 +212,6 @@ class Puzzle:
 
 	def on_set_room(self):
 		pass
-
-	def set_room(self, room):
-		self.room = room
-		self.on_set_room()
 
 	def on_enter_room(self):
 		pass

puzzle/atomix.py
@@ -234,11 +234,11 @@ class AtomixPuzzle(Puzzle):
 
 	@property
 	def goal_molecule(self):
-		return self.goal_molecules[self.room.idx]
+		return self.goal_molecules[room.idx]
 
 	@goal_molecule.setter
 	def goal_molecule(self, molecule):
-		self.goal_molecules[self.room.idx] = molecule
+		self.goal_molecules[room.idx] = molecule
 
 		atom_ids = self.get_goal_molecule_atom_ids()
 		uniq_atom_ids = []
@@ -257,15 +257,15 @@ class AtomixPuzzle(Puzzle):
 			atom_base = self.get_atom_base(atom_id)
 			atom_base_counters[atom_base] += 1
 			atom_indexes[atom_id] = None if len(atom_base_ids[atom_base]) == 1 else atom_base_counters[atom_base]
-		self.atom_indexes_per_room[self.room.idx] = atom_indexes
+		self.atom_indexes_per_room[room.idx] = atom_indexes
 
 	@property
 	def atom_indexes(self):
-		return self.atom_indexes_per_room[self.room.idx]
+		return self.atom_indexes_per_room[room.idx]
 
 	@atom_indexes.setter
 	def atom_indexes(self, indexes):
-		self.atom_indexes_per_room[self.room.idx] = indexes
+		self.atom_indexes_per_room[room.idx] = indexes
 
 	def get_goal_molecule_atom_ids(self):
 		return [atom_id for row in self.goal_molecule for atom_id in row if atom_id != '']
@@ -279,7 +279,7 @@ class AtomixPuzzle(Puzzle):
 		return self.get_atom_base(atom_id) + index_str
 	
 	def get_current_lift_molecule(self):
-		room_lifts = self.Globals.get_actors_in_room(lifts)
+		room_lifts = get_actors_in_room(lifts)
 
 		x_min = y_min = 1000
 		x_max = y_max = -1
@@ -326,7 +326,7 @@ class AtomixPuzzle(Puzzle):
 
 	def scramble(self):
 		# move all lifts until possible without repetition
-		room_lifts = self.Globals.get_actors_in_room(lifts)
+		room_lifts = get_actors_in_room(lifts)
 		was_scrambled = False
 
 		visited_lift_molecules = [self.goal_molecule]
@@ -336,7 +336,7 @@ class AtomixPuzzle(Puzzle):
 					neigh_presense = 0
 					for dir in 'lrdu':
 						neigh = apply_diff(lift.c, dir_diffs[dir])
-						if not self.Globals.is_cell_in_room(neigh) or self.map[neigh] != CELL_VOID or is_cell_in_actors(neigh, lifts):
+						if not is_cell_in_room(neigh) or self.map[neigh] != CELL_VOID or is_cell_in_actors(neigh, lifts):
 							neigh_presense += diff_factors[dir]
 					scramble_dirs = [dir for dir in scramble_presense_dirs[neigh_presense] if dir != ' ']
 					for scramble_dir in scramble_dirs:
@@ -406,7 +406,7 @@ class AtomixPuzzle(Puzzle):
 		self.set_area_from_config(align_to_center=True)
 
 		# replace floor with void and fill unused space with walls
-		for cell in self.room.cells:
+		for cell in room.cells:
 			self.map[cell] = CELL_VOID if self.is_in_area(cell) else CELL_FLOOR if flags.MULTI_ROOMS else CELL_WALL
 
 		if not flags.MULTI_ROOMS:
@@ -470,7 +470,7 @@ class AtomixPuzzle(Puzzle):
 	def on_update(self, level_time):
 		if not active_inplace_animation_actors:
 			if self.draw_solved_mode != self.last_draw_solved_mode:
-				room_lifts = self.Globals.get_actors_in_room(lifts)
+				room_lifts = get_actors_in_room(lifts)
 				for lift in room_lifts:
 					lift.cell_to_draw = lift.goal_cell if self.draw_solved_mode else None
 				self.last_draw_solved_mode = self.draw_solved_mode

puzzle/barrel.py
@@ -38,8 +38,8 @@ class BarrelPuzzle(Puzzle):
 
 	def on_enter_room(self):
 		# prepare some invariant data
-		self.num_total_cells = self.room.size_x * self.room.size_y
-		self.plate_cells = [ tuple(cell) for cell in argwhere(self.map == CELL_PLATE) if self.Globals.is_cell_in_room(cell) ]
+		self.num_total_cells = room.size_x * room.size_y
+		self.plate_cells = [ tuple(cell) for cell in argwhere(self.map == CELL_PLATE) if is_cell_in_room(cell) ]
 		self.plate_cells.sort()
 		self.stock_barrel_cells = [ barrel.c for barrel in self.get_room_barrels() ]
 		self.stock_barrel_cells.sort()
@@ -49,7 +49,7 @@ class BarrelPuzzle(Puzzle):
 		self.reset_solution_data()
 
 	def get_room_barrels(self):
-		return [ barrel for barrel in barrels if self.Globals.is_actor_in_room(barrel) ]
+		return [ barrel for barrel in barrels if is_actor_in_room(barrel) ]
 
 	def show_map(self, descr=None, char_cell=None, barrel_cells=None):
 		orig_barrels = barrels.copy()
@@ -446,7 +446,7 @@ class BarrelPuzzle(Puzzle):
 		char.c = None
 
 		# 1) initialize entire room to WALL
-		for cell in self.room.cells:
+		for cell in room.cells:
 			self.map[cell] = CELL_WALL
 
 		# 2) place room plates randomly or in good positions, as the number of barrels
@@ -614,7 +614,7 @@ class BarrelPuzzle(Puzzle):
 			plate_cells = []
 
 			# 1) initialize entire room to WALL
-			for cell in self.room.cells:
+			for cell in room.cells:
 				self.map[cell] = CELL_WALL
 
 			# 2) place room plates randomly or in good positions, as the number of barrels

puzzle/gate.py
@@ -157,51 +157,51 @@ class GatePuzzle(Puzzle):
 
 	@property
 	def num_plates(self):
-		return self.room_values[self.room.idx][0]
+		return self.room_values[room.idx][0]
 
 	@num_plates.setter
 	def num_plates(self, num_plates):
-		self.room_values[self.room.idx][0] = num_plates
+		self.room_values[room.idx][0] = num_plates
 
 	@property
 	def num_gates(self):
-		return self.room_values[self.room.idx][1]
+		return self.room_values[room.idx][1]
 
 	@num_gates.setter
 	def num_gates(self, num_gates):
-		self.room_values[self.room.idx][1] = num_gates
+		self.room_values[room.idx][1] = num_gates
 
 	@property
 	def finish_cell(self):
-		return self.room_values[self.room.idx][2]
+		return self.room_values[room.idx][2]
 
 	@finish_cell.setter
 	def finish_cell(self, finish_cell):
-		self.room_values[self.room.idx][2] = finish_cell
+		self.room_values[room.idx][2] = finish_cell
 
 	@property
 	def plate_cells(self):
-		return self.room_values[self.room.idx][3]
+		return self.room_values[room.idx][3]
 
 	@plate_cells.setter
 	def plate_cells(self, plate_cells):
-		self.room_values[self.room.idx][3] = plate_cells
+		self.room_values[room.idx][3] = plate_cells
 
 	@property
 	def gate_cells(self):
-		return self.room_values[self.room.idx][4]
+		return self.room_values[room.idx][4]
 
 	@gate_cells.setter
 	def gate_cells(self, gate_cells):
-		self.room_values[self.room.idx][4] = gate_cells
+		self.room_values[room.idx][4] = gate_cells
 
 	@property
 	def attached_plate_gate_idxs(self):
-		return self.room_values[self.room.idx][5]
+		return self.room_values[room.idx][5]
 
 	@attached_plate_gate_idxs.setter
 	def attached_plate_gate_idxs(self, attached_plate_gate_idxs):
-		self.room_values[self.room.idx][5] = attached_plate_gate_idxs
+		self.room_values[room.idx][5] = attached_plate_gate_idxs
 
 	def store_level(self, stored_level):
 		stored_level["room_values"] = self.room_values

puzzle/hero.py
@@ -52,7 +52,7 @@ class HeroPuzzle(Puzzle):
 		num_slots = self.area.size_x - 1
 
 		self.set_area_border_walls()
-		if self.area.x1 > self.room.x1:
+		if self.area.x1 > room.x1:
 			self.map[self.area.x1 - 1, (self.area.y1 + self.area.y2) // 2] = CELL_GATE1
 
 		for cell in self.area.cells:
@@ -70,4 +70,4 @@ class HeroPuzzle(Puzzle):
 			else:
 				self.generate_random_nonsolvable_floor_cell(cell)
 
-		self.map[self.room.x1, self.room.y2] = CELL_FINISH
+		self.map[room.x1, room.y2] = CELL_FINISH

puzzle/memory.py
@@ -61,7 +61,7 @@ class MemoryPuzzle(Puzzle):
 		return True
 
 	def is_solved(self):
-		return not self.room_memory_pairs[self.room.idx]
+		return not self.room_memory_pairs[room.idx]
 
 	def is_time_to_reveal(self):
 		return self.reveal_time > self.level_time
@@ -79,7 +79,7 @@ class MemoryPuzzle(Puzzle):
 		if self.memory_map[self.open_cell2] == pair_idx:
 			self.memory_map[self.open_cell1] = MEMORY_PUZZLE_VALUE_OUTSIDE
 			self.memory_map[self.open_cell2] = MEMORY_PUZZLE_VALUE_OUTSIDE
-			del self.room_memory_pairs[self.room.idx][pair_idx]
+			del self.room_memory_pairs[room.idx][pair_idx]
 		self.unset_open_cells()
 
 	def press_cell(self, cell, button=None):
@@ -131,7 +131,7 @@ class MemoryPuzzle(Puzzle):
 				debug(3, "	%s" % str(cell2))
 				self.memory_map[cell2] = pair_idx
 				memory_pairs[pair_idx] = (cell1, cell2)  # or True
-		self.room_memory_pairs[self.room.idx] = memory_pairs
+		self.room_memory_pairs[room.idx] = memory_pairs
 
 		self.Globals.set_char_cell(self.get_empty_central_cell())
 

puzzle/minotaur.py
@@ -28,19 +28,19 @@ class MinotaurPuzzle(Puzzle):
 
 	@property
 	def minotaur_cell(self):
-		return self.minotaur_cells[self.room.idx]
+		return self.minotaur_cells[room.idx]
 
 	@minotaur_cell.setter
 	def minotaur_cell(self, cell):
-		self.minotaur_cells[self.room.idx] = cell
+		self.minotaur_cells[room.idx] = cell
 
 	@property
 	def goal_cell(self):
-		return self.goal_cells[self.room.idx]
+		return self.goal_cells[room.idx]
 
 	@goal_cell.setter
 	def goal_cell(self, cell):
-		self.goal_cells[self.room.idx] = cell
+		self.goal_cells[room.idx] = cell
 
 	def is_lost(self):
 		return self._is_lost
@@ -104,9 +104,9 @@ class MinotaurPuzzle(Puzzle):
 
 	def set_goal_and_finish_cell(self, goal_cell):
 		self.goal_cell = goal_cell
-		if self.area.size != self.room.size and self.Globals.is_cell_accessible(self.room.cell11):
-			finish_cell = self.Globals.get_farthest_accessible_cell(self.room.cell11)
-			self.Globals.create_portal(goal_cell, self.room.cell11)
+		if self.area.size != room.size and self.Globals.is_cell_accessible(room.cell11):
+			finish_cell = self.Globals.get_farthest_accessible_cell(room.cell11)
+			self.Globals.create_portal(goal_cell, room.cell11)
 		else:
 			finish_cell = goal_cell
 		self.map[finish_cell] = CELL_FINISH

commit b8cd38f13da8def894a170b5584b62ceb5df96ef

Puzzle: Don't import is_cell_in_area via Globals, use it directly from room
It was a minor ommission when room related functions were refactored.

Fixes: fbfa8aaae42e ("Refactor global room and related functions into separate room.py")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -955,7 +955,6 @@ def toggle_actor_phased(actor):
 	play_sound(sound_name)
 
 class Globals:
-	is_cell_in_area = is_cell_in_area
 	get_actor_neighbors = get_actor_neighbors
 	get_all_neighbors = get_all_neighbors
 	get_bg_image = get_bg_image

puzzle/__init__.py
@@ -151,10 +151,10 @@ class Puzzle:
 		self.area.y_range = range(self.area.y1, self.area.y2 + 1)
 
 	def is_in_area(self, cell):
-		return self.Globals.is_cell_in_area(cell, self.area.x_range, self.area.y_range)
+		return is_cell_in_area(cell, self.area.x_range, self.area.y_range)
 
 	def is_in_room(self, cell):
-		return self.Globals.is_cell_in_area(cell, room.x_range, room.y_range)
+		return is_cell_in_room(cell)
 
 	def set_area_border_walls(self, width=1):
 		for cell in room.cells: