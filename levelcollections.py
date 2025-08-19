mig0/puzzle-dungeon

commit 009cd03a0804cb5244964b67f17b6090561f8498

Add 12 MirrorPuzzle levels and maps
These are manually crafted levels that feature most of the MirrorPuzzle
features; static and moveable mirrors, mirrors with fixed and changeable
orientation and activeness, mirrors on barrels, lifts and carts, gates,
traps, locks, glasses, and portals.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

levelcollections.py
@@ -12,6 +12,12 @@ collections = [
 		"icon": 'default/gate0.png',
 	},
 	{
+		"id": "mirror",
+		"name": "Mirror levels",
+		"n": 2.901,
+		"icon": 'default/mirror.png',
+	},
+	{
 		"id": "switchbox",
 		"name": "SwitchBoxPuzzle levels",
 		"n": 3.01,

levels.py
@@ -118,6 +118,133 @@ levels = [
 		},
 	},
 	{
+		"n": 2.901,
+		"map_file": "mirror/simple-barrels.map",
+		"name": "Simple Barrels with Mirrors",
+		"num_enemies": 0,
+		"theme": "classic",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.902,
+		"map_file": "mirror/locked-line.map",
+		"name": "Locked Line",
+		"num_enemies": 0,
+		"theme": "moss",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.903,
+		"map_file": "mirror/hike.map",
+		"name": "Hike",
+		"num_enemies": 0,
+		"theme": "default",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.904,
+		"map_file": "mirror/barrel-zsb-3x3.map",
+		"name": "Screwed Barrels",
+		"num_enemies": 0,
+		"theme": "ancient2",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.905,
+		"map_file": "mirror/gate-barrels.map",
+		"name": "Gates with Barrels",
+		"num_enemies": 0,
+		"theme": "modern1",
+		"music": "gaming_spirit",
+		"char_health": 100,
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.906,
+		"map_file": "mirror/sunbeam.map",
+		"name": "Sunbeam",
+		"num_enemies": 0,
+		"theme": "classic",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"bg_image": "bg/palace-mirror-room-yellow.webp",
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.907,
+		"map_file": "mirror/radiant.map",
+		"name": "Radiant",
+		"num_enemies": 0,
+		"theme": "classic",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"bg_image": "bg/palace-mirror-room-purple.webp",
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.908,
+		"map_file": "mirror/beamy.map",
+		"name": "Beamy",
+		"num_enemies": 0,
+		"theme": "default",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"bg_image": "bg/palace-mirror-room-yellow.webp",
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.909,
+		"map_file": "mirror/reflections.map",
+		"name": "Reflections",
+		"num_enemies": 0,
+		"theme": "default",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"bg_image": "bg/palace-mirror-room-purple.webp",
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.910,
+		"map_file": "mirror/reflectivity.map",
+		"name": "Reflectivity",
+		"num_enemies": 0,
+		"theme": "ancient2",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"bg_image": "bg/palace-mirror-room-yellow.webp",
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.911,
+		"map_file": "mirror/6x3-1.map",
+		"name": "Mincraft Mirror #1",
+		"num_enemies": 0,
+		"theme": "minecraft",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"bg_image": "bg/minecraft-mirror-grot.webp",
+		"mirror_puzzle": True,
+	},
+	{
+		"n": 2.912,
+		"map_file": "mirror/6x3-2.map",
+		"name": "Mincraft Mirror #2",
+		"num_enemies": 0,
+		"theme": "minecraft",
+		"music": "gaming_spirit",
+		"char_health": None,
+		"bg_image": "bg/minecraft-mirror-grot.webp",
+		"mirror_puzzle": True,
+	},
+	{
 		"n": 3.01,
 		"map_file": "switchbox/001.map",
 		"title": "SwitchBox - level 1",

maps/mirror/6x3-1.map
@@ -1,0 +1,30 @@
+# Dungeon MirrorPuzzle 6x3-1 map 13x13
+·············
+·············
+·············
+·····✦◎◎·····
+·····◎◎◎·····
+·····◎◎◎·····
+·····◎◎◎·····
+·····◎◎◎·····
+·····◎◎✧·····
+·············
+·············
+·············
+······•······
+\ 0*
+\ 0*
+- 1
+/* 0*
+-* 0*
+-* 0*
+\* 0*
+/ 1
+/* 1*
+\ 1*
+\* 0*
+\* 2
+-* 0
+\ 1
+/* 0*
+| 1

maps/mirror/6x3-2.map
@@ -1,0 +1,30 @@
+# Dungeon MirrorPuzzle 6x3-2 map 13x13
+·············
+·············
+·············
+·····◎✧◎·····
+·····◎◎◎·····
+·····◎◎◎·····
+·····◎◎◎·····
+·····◎◎◎·····
+·····◎◎✦·····
+·············
+·············
+·············
+·····►◦◄·····
+\ 0*
+\ 0*
+/ 0*
+\ 1*
+- 1
+/ 0*
+| 1
+/ 1
+/ 0*
+\ 0*
+/ 0*
+\ 0*
+/ 0*
+\* 0*
+-* 0*
+- 0*

maps/mirror/barrel-zsb-3x3.map
@@ -1,0 +1,24 @@
+# Dungeon MirrorPuzzle glass+barrels/zsb+traps map 13x13
+▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒░░░▬░░░░░░✧▒
+▒░▓▓⬧▓▓▓▓▓▓░▒
+▒░▓☻••••••▓░▒
+▒░▓•░•░▬░•▓░▒
+▒░▓•▬•▬•••▓░▒
+▒░⬧•░▬░▬░•⬧░▒
+▒░▓•••▬•▬•▓░▒
+▒░▓•░▬░•░•▓░▒
+▒░▓•••••••▓░▒
+▒░▓▓▓▓▓▓⬧▓▓░▒
+▒✦░░░░░░▬░░░▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒
+\* 1
+\ 1
+/ 1
+/ 1
+/* 1
+/* 1
+/ 1
+/ 1
+/ 1
+/* 1

maps/mirror/beamy.map
@@ -1,0 +1,49 @@
+# Dungeon MirrorPuzzle Mazelog Beamy map 13x13
+✥············
+·············
+·············
+···✦◎◎·◎◎◎···
+·····◎··◎◎···
+···◎◎◎··◎◎···
+······✧······
+···◎·◎·◎◎◎···
+···◎◎◎◎◎◎··◉·
+···◎◎◎◎◎◎◎···
+·············
+········◉····
+············◉
+\ 0*
+/ 0*
+\ 0*
+/ 0*
+/ 0*
+\ 0*
+\ 0*
+\ 0*
+/ 0*
+/ 0*
+\ 0*
+\ 0*
+/ 0*
+\ 0*
+\ 0*
+\ 0*
+/ 0*
+/ 0*
+/ 0*
+\ 0*
+/ 0*
+\ 0*
+\ 0*
+\ 0*
+\ 0*
+\ 0*
+\ 0*
+\ 0*
+\ 0*
+\ 0*
+/ 0*
+
+Author: David Johnson-Davies <david@mazelog.com>
+Link: http://www.mazelog.com/show?JQ
+Solution: 1 29 31 38 37 16 20 34 33 5 3 10 14 49 46 25

maps/mirror/gate-barrels.map
@@ -1,0 +1,23 @@
+# Dungeon MirrorPuzzle gates+barrels map 13x13
+▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓✧▓•••••••••▓
+▓•▓•▓▓▓▓▓▓▓☻▓
+▓★▓•▓▓▓▓▓▓▓•▓
+▓•••••••••••▓
+▓•▓▓▓•••▓▓▓▓▓
+▓•▓▓▓•••▓••𝟘▓
+▓•••••••▓•▬■▓
+▓𝟘▓▓▓▓▬▓▓•▬•▓
+▓☆▓•••••▬•••▓
+▓••••▬••▬•▓▓▓
+▓✦▓••••••••⎵▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓
+1
+0
+/* 1
+/* 1*
+/* 1
+/* 1
+/* 1
+/* 1
+0 1

maps/mirror/hike.map
@@ -1,0 +1,24 @@
+# Dungeon MirrorPuzzle gates+locks+portals+glass+barrels map 13x13
+☻•••••𝟘••••★⎵
+★••✦▬•▓•••▬•★
+²★•••••••••★¹
+▓▓▓▓▓▓░▓▓▓▓▓▓
+⎵𝟚•••••••••★⎵
+𝟚••••••••••▬★
+•••••••••••𝟙¹
+••••••••••••★
+⎵••••••••••••
+•••••••••••••
+••••••••••••▓
+𝟚▬▓••••••••★𝟙
+²𝟚••••𝟘••••▓✧
+1
+0
+-* 1
+|* 1
+-* 1
+/* 1
+1 3
+5 8
+4
+0

commit 9a51101bc2ff161c4a7a18a3b0c781a8ce02ca6b

Correct name of MirrorPuzzle level collection
Use name "MirrorPuzzle" and not "Mirror" for consistency with other
collections.

Fixes: 009cd03a0804 ("Add 12 MirrorPuzzle levels and maps")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

levelcollections.py
@@ -13,7 +13,7 @@ collections = [
 	},
 	{
 		"id": "mirror",
-		"name": "Mirror levels",
+		"name": "MirrorPuzzle levels",
 		"n": 2.901,
 		"icon": 'default/mirror.png',
 	},