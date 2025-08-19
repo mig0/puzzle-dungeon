mig0/puzzle-dungeon

commit 892cb67895473d3ceaf17e7b3576459e0068a30c

Refactor translate related functions into separate translate.py
Move these functions from main to new translate.py:
	_
	autodetect_lang

Add these new functions:
	set_lang

Now puzzles and other modules may use the mechanism to translate strings.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -1,6 +1,5 @@
 import os
 import io
-import re
 import sys
 import random
 import pygame
@@ -14,7 +13,7 @@ from unicodedata import bidirectional
 from constants import *
 from teestream import *
 from leveltools import *
-from translations import *
+from sizetools import *
 from cellactor import *
 from objects import *
 from common import *
@@ -27,74 +26,14 @@ from drop import draw_status_drops
 from flags import flags
 from puzzle import create_puzzle
 from solution import solution, set_solution_funcs
-from sizetools import *
 from joystick import scan_joysticks_and_state, emulate_joysticks_press_key, get_joysticks_arrow_keys
 from clipboard import clipboard
+from translate import _, set_lang
 from statusmessage import reset_status_messages, set_status_message, draw_status_message
 
 # set data dir and default encoding for the whole program
 pgzero.loaders.set_root(DATA_DIR)
 sys.stdout.reconfigure(encoding='utf-8')
-
-lang = 'en'
-
-def autodetect_lang():
-	global lang
-
-	lang = 'en'
-	try:
-		if 'LANG' in os.environ:
-			lang0 = (os.environ['LANG'])[0:2]
-			if lang0 in translations:
-				lang = lang0
-	except:
-		pass
-
-# translate key like "default-goal" or embedded keys like "{reach-finish} {in-word} 5 {seconds-word}"
-def _(str_key, disable_bidi=False):
-	if str_key == '' or str_key is None:
-		return ''
-
-	def is_rtl(word):
-		for ch in word:
-			b = bidirectional(ch)
-			if b in ('R', 'AL'):
-				return True
-			if b in ('L', 'EN', 'AN'):
-				return False
-		return False  # default to LTR if no strong character
-
-	def bidi(str):
-		"""BiDi simulation using word-level RTL detection, preserving original whitespace separators."""
-		nonlocal disable_bidi
-		if disable_bidi or not str or lang != 'he' or not any(bidirectional(ch) in ('R', 'AL') for ch in str):
-			return str
-
-		parts = re.findall(r'\S+|\s+', str)
-		tokens = []
-
-		for part in parts:
-			if part.isspace():
-				tokens.append((part, False))  # separator
-			else:
-				tokens.append((part, is_rtl(part)))
-
-		if is_rtl(str):
-			tokens = reversed(tokens)
-
-		return ''.join(
-			word[::-1] if is_rtl else word
-			for word, is_rtl in tokens
-		)
-
-	# replace all {sub-key} occurrences (flat, no nesting)
-	str_key = re.sub(r'{([^{}]+)}', lambda m: _(m.group(1), True), str_key)
-
-	str = translations[lang].get(str_key) or translations['en'].get(str_key) or str_key
-
-	return bidi(str)
-
-autodetect_lang()
 
 def load_map(filename_or_stringio, special_cell_types={}):
 	global map
@@ -1661,7 +1600,6 @@ def change_solution_move_delay(is_reset, is_dec, is_inc):
 
 def handle_press_key():
 	global is_main_screen
-	global lang
 	global is_move_animate_enabled, is_level_intro_enabled, is_sound_enabled
 
 	# apply workaround for the invalid syntax keyboard.return in python
@@ -1693,11 +1631,11 @@ def handle_press_key():
 
 	if keyboard.rshift:
 		if keyboard.e:
-			lang = 'en'
+			set_lang('en')
 		if keyboard.r:
-			lang = 'ru'
+			set_lang('ru')
 		if keyboard.h:
-			lang = 'he'
+			set_lang('he')
 
 		if keyboard.l:
 			is_level_intro_enabled = not is_level_intro_enabled

translate.py
@@ -1,0 +1,66 @@
+import os
+import re
+from translations import *
+
+lang = 'en'
+
+def set_lang(_lang):
+	global lang
+	lang = _lang
+
+def autodetect_lang():
+	lang = 'en'
+	try:
+		if 'LANG' in os.environ:
+			lang0 = (os.environ['LANG'])[0:2]
+			if lang0 in translations:
+				lang = lang0
+	except:
+		pass
+	set_lang(lang)
+
+# translate key like "default-goal" or embedded keys like "{reach-finish} {in-word} 5 {seconds-word}"
+def _(str_key, disable_bidi=False):
+	if str_key == '' or str_key is None:
+		return ''
+
+	def is_rtl(word):
+		for ch in word:
+			b = bidirectional(ch)
+			if b in ('R', 'AL'):
+				return True
+			if b in ('L', 'EN', 'AN'):
+				return False
+		return False  # default to LTR if no strong character
+
+	def bidi(str):
+		"""BiDi simulation using word-level RTL detection, preserving original whitespace separators."""
+		nonlocal disable_bidi
+		if disable_bidi or not str or lang != 'he' or not any(bidirectional(ch) in ('R', 'AL') for ch in str):
+			return str
+
+		parts = re.findall(r'\S+|\s+', str)
+		tokens = []
+
+		for part in parts:
+			if part.isspace():
+				tokens.append((part, False))  # separator
+			else:
+				tokens.append((part, is_rtl(part)))
+
+		if is_rtl(str):
+			tokens = reversed(tokens)
+
+		return ''.join(
+			word[::-1] if is_rtl else word
+			for word, is_rtl in tokens
+		)
+
+	# replace all {sub-key} occurrences (flat, no nesting)
+	str_key = re.sub(r'{([^{}]+)}', lambda m: _(m.group(1), True), str_key)
+
+	str = translations[lang].get(str_key) or translations['en'].get(str_key) or str_key
+
+	return bidi(str)
+
+autodetect_lang()

commit 0dad14b3e96dd1edf0d0b4fbd4a29c0e8ec29656

Fix bidi translations
While refatoring to new translate.py the import statement was not moved.
Fix this.

Fixes: 892cb6789547 ("Refactor translate related functions into separate translate.py")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -9,7 +9,6 @@ from pgzero.constants import keys
 from numpy import ndarray
 from copy import deepcopy
 from random import randint, choice, shuffle
-from unicodedata import bidirectional
 from constants import *
 from teestream import *
 from leveltools import *

translate.py
@@ -1,5 +1,6 @@
 import os
 import re
+from unicodedata import bidirectional
 from translations import *
 
 lang = 'en'