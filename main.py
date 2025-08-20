mig0/puzzle-dungeon

commit ac40179fff427f3ce1e01979172a9b892183c420

Improve detection of outer wall
Outer wall is not just the one with only neighbour walls, but neighbours
may also be all or some VOID cells.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -404,7 +404,7 @@ def is_outer_wall(cell):
 		return False
 
 	for neigh in get_all_neighbors(cell):
-		if is_cell_in_map(neigh) and map[neigh] not in CELL_WALL_TYPES:
+		if is_cell_in_map(neigh) and map[neigh] not in (*CELL_WALL_TYPES, CELL_VOID):
 			return False
 	return True
 

commit c59d7ca71d46eb7c8998273f0f069b8aff8c9ad5

Restore the old detection of outer wall as the default
There is a need to have an old detection of outer wall in AtomixPuzzle,
make it as the default again. The new implementation is now available for
BarrelPuzzle via void_is_like_wall flag.

Fixes: ac40179fff42 ("Improve detection of outer wall")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -400,12 +400,14 @@ def debug_map(level=0, descr=None, full_format=False, full=True, clean=True, com
 def is_cell_in_map(cell):
 	return is_cell_in_area(cell, MAP_X_RANGE, MAP_Y_RANGE)
 
-def is_outer_wall(cell):
+def is_outer_wall(cell, void_is_like_wall=False):
 	if map[cell] not in CELL_WALL_TYPES:
 		return False
 
+	wall_types = (*CELL_WALL_TYPES, CELL_VOID) if void_is_like_wall else CELL_WALL_TYPES
+
 	for neigh in get_all_neighbors(cell):
-		if is_cell_in_map(neigh) and map[neigh] not in (*CELL_WALL_TYPES, CELL_VOID):
+		if is_cell_in_map(neigh) and map[neigh] not in wall_types:
 			return False
 	return True
 
@@ -415,10 +417,10 @@ def replace_outer_walls(*cell_types):
 			if map[cx, cy] == CELL_OUTER_WALL:
 				map[cx, cy] = choice(cell_types)
 
-def convert_outer_walls(cell_type=None):
+def convert_outer_walls(cell_type=None, void_is_like_wall=False):
 	for cy in MAP_Y_RANGE:
 		for cx in MAP_X_RANGE:
-			if is_outer_wall((cx, cy)):
+			if is_outer_wall((cx, cy), void_is_like_wall=void_is_like_wall):
 				map[cx, cy] = CELL_OUTER_WALL
 
 	if cell_type is not None:

puzzle/barrel.py
@@ -539,7 +539,7 @@ class BarrelPuzzle(Puzzle):
 		self.on_generate_map()
 
 	def on_generate_map(self):
-		self.Globals.convert_outer_walls(CELL_VOID if "bg_image" in self.level else None)
+		self.Globals.convert_outer_walls(CELL_VOID if "bg_image" in self.level else None, True)
 
 	def pull_barrel_randomly(self, barrel, visited_cell_pairs, num_moves):
 		idx = barrels.index(barrel)