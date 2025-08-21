mig0/puzzle-dungeon

commit 2618868ad0f2354903a803f48e49e8da054f9035

Handle Space centrally to mean press char cell rather than in each puzzle
Now Space when the cursor is not active (i.e. char or lift is active)
calls puzzle.press_cell(char.c).

Update all 6 puzzles that handle Space to implement press_cell instead of
listening to Space. Some of the puzzles already do this, then just remove
the keyboard handling in them.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/gate.py
@@ -443,11 +443,12 @@ class GatePuzzle(Puzzle):
 		return self.attached_plate_gate_idxs
 
 	def on_press_key(self, keyboard):
-		if keyboard.space:
-			self.press_plate(char.c)
 		if keyboard.kp_enter:
 			solution = self.find_map_solution(char.c)
 			if solution:
 				pressed_plate_cells = tuple(self.plate_cells[idx] for idx in solution.pressed_plates)
 				print("Press plates:", *pressed_plate_cells)
-		return False
+
+	def press_cell(self, cell, button=None):
+		self.press_plate(char.c)
+

puzzle/minotaur.py
@@ -225,10 +225,9 @@ class MinotaurPuzzle(Puzzle):
 	def on_draw(self, mode):
 		self.minotaur.draw()
 
-	def on_press_key(self, keyboard):
-		if keyboard.space:
-			# skip move
-			self.make_minotaur_move()
+	def press_cell(self, cell, button=None):
+		# skip move
+		self.make_minotaur_move()
 
 	def on_prepare_enter_cell(self):
 		self.make_minotaur_move()

main.py
@@ -1676,6 +1676,9 @@ def handle_press_key():
 	if DEBUG_LEVEL > 0 and cursor_was_active and not cursor.is_active():
 		set_status_message(priority=0)
 
+	if keyboard.space and not cursor_was_active:
+		puzzle.press_cell(char.c)
+
 	if keyboard.home:
 		puzzle.press_cell(cursor.selected_actor.c, 1)
 	if keyboard.end:

puzzle/color.py
@@ -95,7 +95,4 @@ class ColorPuzzle(Puzzle):
 
 		return True
 
-	def on_press_key(self, keyboard):
-		if keyboard.space and self.map[char.c] == CELL_PLATE:
-			self.press_plate(char.c)
 

puzzle/fifteen.py
@@ -139,13 +139,7 @@ class FifteenPuzzle(Puzzle):
 		self.move(cell, empty_cell, FIFTEEN_PUZZLE_MOVE_NEIGHBOUR_ONLY)
 		return True
 
-	def press_char_cell(self):
-		self.press_cell(char.c)
-
 	def on_press_key(self, keyboard):
-		if keyboard.space:
-			self.press_char_cell()
-
 		self.draw_solved_mode = keyboard.kp_enter and not self.draw_solved_mode
 
 	def set_char_opacity_if_needed(self):

puzzle/memory.py
@@ -168,9 +168,6 @@ class MemoryPuzzle(Puzzle):
 		return None
 
 	def on_press_key(self, keyboard):
-		if keyboard.space:
-			self.press_cell(char.c)
-
 		if keyboard.kp_enter:
 			self.reveal_time = self.level_time + 5
 

puzzle/rotatepic.py
@@ -89,12 +89,7 @@ class RotatepicPuzzle(Puzzle):
 	def press_cell(self, cell, button=None):
 		return self.rotate_cell(cell, CLOCKWISE if button in (None, 3, 5) else UPSIDEDOWN if button == 2 else COUNTERCLOCKWISE)
 
-	def press_char_cell(self, clockwise=True):
-		self.rotate_cell(char.c, CLOCKWISE if clockwise else COUNTERCLOCKWISE)
-
 	def on_press_key(self, keyboard):
-		if keyboard.space:
-			self.press_char_cell()
 		if keyboard.backspace:
 			self.scramble()
 		self.draw_solved_mode = keyboard.kp_enter and not self.draw_solved_mode

commit 1575b9b339f72ec3bbc82b6bac4a031d5329e657

GatePuzzle: Fix sounds during solution
The press_cell callback is always expected to return boolean whether the
press was handled. Not returning True on plate presses during solution
produces redundant error sound. Fix this omission.

Do the same fix in MinotaurPuzzle (it should always return True) although
press_cell is not currently used in MinotaurPuzzle solution, so there is
no sound problem in it.

Fixes: 2618868ad0f2 ("Handle Space centrally to mean press char cell rather than in each puzzle")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/gate.py
@@ -340,10 +340,11 @@ class GatePuzzle(Puzzle):
 
 	def press_plate(self, cell):
 		if self.map[cell] != CELL_PLATE:
-			return
+			return False
 		plate_idx = self.plate_cells.index(cell)
 		for gate_idx in self.attached_plate_gate_idxs[plate_idx]:
 			self.toggle_gate(self.gate_cells[gate_idx])
+		return True
 
 	def generate_random_solvable_room(self, accessible_cells, finish_cell):
 		self.num_plates = self.parse_config_num("num_plates", DEFAULT_NUM_GATE_PUZZLE_PLATES)
@@ -446,7 +447,7 @@ class GatePuzzle(Puzzle):
 		return self.attached_plate_gate_idxs
 
 	def press_cell(self, cell, button=None):
-		self.press_plate(char.c)
+		return self.press_plate(char.c)
 
 	def find_solution_func(self):
 		solution = self.find_map_solution(char.c)

puzzle/minotaur.py
@@ -256,6 +256,7 @@ class MinotaurPuzzle(Puzzle):
 	def press_cell(self, cell, button=None):
 		# skip move
 		self.make_minotaur_move()
+		return True
 
 	def on_prepare_enter_cell(self):
 		if char.c == self.goal_cell and self.map[char.c] == CELL_PORTAL: