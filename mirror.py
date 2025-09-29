mig0/puzzle-dungeon

commit 034be22f94c154be478da5d8a2dd28276665002b

Move all cell related functions and Area from cellactor to new celltools
To reduce circular dependencies of modules, refactor new module celltools
from cellactor. This includes class Area and functions:
	cmp
	product
	apply_diff
	cell_diff
	cell_dir
	cell_to_pos_offset
	cell_to_pos_00
	cell_to_pos
	pos_to_cell
	cell_distance
	sort_cells

Now module room is light, so cellactor may import game (that includes
room) at the top.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

mirror.py
@@ -1,6 +1,6 @@
 import pygame
 from typing import Union
-from cellactor import *
+from cellactor import CellActor
 from common import die
 from game import game
 from constants import MIRROR_INACTIVE_OPACITY, MIRROR_ORIENTATION_CHARS

cellactor.py
@@ -3,10 +3,9 @@ from pgzero.actor import Actor, POS_TOPLEFT, ANCHOR_CENTER, transform_anchor
 from pgzero.animation import *
 from pgzero import loaders
 from typing import Union, Tuple
-from sizetools import CELL_W, CELL_H
 from config import ARROW_KEYS_RESOLUTION, ACTOR_PHASED_OPACITY
-from constants import DIR_NAMES
-from common import die
+from celltools import *
+from game import game
 
 MAX_ALPHA = 255  # based on pygame
 
@@ -15,112 +14,9 @@ NONE_SURFACE = pygame.Surface((0, 0))
 
 active_inplace_animation_actors = []
 
-def cmp(n1, n2):
-	return 1 if n1 > n2 else 0 if n1 == n2 else -1
-
-def product(x_range, y_range, run_by_y=False):
-	if run_by_y:
-		for x in x_range:
-			for y in y_range:
-				yield (x, y)
-	else:
-		for y in y_range:
-			for x in x_range:
-				yield (x, y)
-
-def apply_diff(orig, diff, subtract=False, factor=1):
-	if subtract:
-		factor = -factor
-	return (orig[0] + diff[0] * factor, orig[1] + diff[1] * factor)
-
-def cell_diff(cell1, cell2, reverse=False, assert_adjacent=False):
-	diff = (cell2[0] - cell1[0], cell2[1] - cell1[1])
-	if reverse:
-		diff = (-diff[0], -diff[1])
-	if assert_adjacent and diff not in DIR_NAMES:
-		die("Cells %s and %s are not adjacent", True)
-	return diff
-
-def cell_dir(cell1, cell2):
-	return (cmp(cell2[0], cell1[0]), cmp(cell2[1], cell1[1]))
-
-def cell_to_pos_offset(cell, offset):
-	return (CELL_W * (cell[0] + offset[0]), CELL_H * (cell[1] + offset[1]))
-
-def cell_to_pos_00(cell):
-	return cell_to_pos_offset(cell, (0, 0))
-
-def cell_to_pos(cell):
-	return cell_to_pos_offset(cell, (0.5, 0.5))
-
-def pos_to_cell(pos):
-	return (pos[0] // CELL_W, pos[1] // CELL_H)
-
-def cell_distance(cell1, cell2):
-	return abs(cell2[0] - cell1[0]) + abs(cell2[1] - cell1[1])
-
-def sort_cells(cells):
-	return sorted(cells, key=lambda cell: (cell[1], cell[0]))
-
 @tweener
 def example(n):
 	return n
-
-class Area:
-	x1 = None
-	y1 = None
-	x2 = None
-	y2 = None
-	size_x = None
-	size_y = None
-	x_range = None
-	y_range = None
-
-	@property
-	def num_cells(self):
-		return len(self.x_range) * len(self.y_range)
-
-	@property
-	def cells(self):
-		return product(self.x_range, self.y_range)
-
-	@property
-	def cell11(self):
-		return (self.x1, self.y1)
-
-	@property
-	def cell12(self):
-		return (self.x1, self.y2)
-
-	@property
-	def cell21(self):
-		return (self.x2, self.y1)
-
-	@property
-	def cell22(self):
-		return (self.x2, self.y2)
-
-	def is_cell_evnevn(self, cell):
-		return (cell[0] - self.x1) % 2 == 0 and (cell[1] - self.y1) % 2 == 0
-
-	def is_cell_evnodd(self, cell):
-		return (cell[0] - self.x1) % 2 == 0 and (cell[1] - self.y1) % 2 == 1
-
-	def is_cell_oddevn(self, cell):
-		return (cell[0] - self.x1) % 2 == 1 and (cell[1] - self.y1) % 2 == 0
-
-	def is_cell_oddodd(self, cell):
-		return (cell[0] - self.x1) % 2 == 1 and (cell[1] - self.y1) % 2 == 1
-
-	def is_cell_inside(self, cell, margin=0):
-		return self.x1 + margin <= cell[0] <= self.x2 - margin and self.y1 + margin <= cell[1] <= self.y2 - margin
-
-	def is_cell_on_margin(self, cell, margin=0):
-		if margin > 0:
-			return self.is_cell_inside(cell) and not self.is_cell_inside(cell, margin)
-		if margin < 0:
-			return not self.is_cell_inside(cell) and self.is_cell_inside(cell, margin)
-		return False
 
 def make_grayscale_image(image):
 	gray_image = pygame.transform.grayscale(image)
@@ -570,4 +466,3 @@ def get_actor_on_cell(cell, actors, include_hidden=None, include_phased=None):
 def is_cell_in_actors(cell, actors, include_hidden=None, include_phased=None):
 	return get_actor_on_cell(cell, actors, include_hidden, include_phased) is not None
 
-from game import game

celltools.py
@@ -1,0 +1,107 @@
+from common import die
+from constants import DIR_NAMES
+from sizetools import CELL_W, CELL_H
+
+def cmp(n1, n2):
+	return 1 if n1 > n2 else 0 if n1 == n2 else -1
+
+def product(x_range, y_range, run_by_y=False):
+	if run_by_y:
+		for x in x_range:
+			for y in y_range:
+				yield (x, y)
+	else:
+		for y in y_range:
+			for x in x_range:
+				yield (x, y)
+
+def apply_diff(orig, diff, subtract=False, factor=1):
+	if subtract:
+		factor = -factor
+	return (orig[0] + diff[0] * factor, orig[1] + diff[1] * factor)
+
+def cell_diff(cell1, cell2, reverse=False, assert_adjacent=False):
+	diff = (cell2[0] - cell1[0], cell2[1] - cell1[1])
+	if reverse:
+		diff = (-diff[0], -diff[1])
+	if assert_adjacent and diff not in DIR_NAMES:
+		die("Cells %s and %s are not adjacent", True)
+	return diff
+
+def cell_dir(cell1, cell2):
+	return (cmp(cell2[0], cell1[0]), cmp(cell2[1], cell1[1]))
+
+def cell_to_pos_offset(cell, offset):
+	return (CELL_W * (cell[0] + offset[0]), CELL_H * (cell[1] + offset[1]))
+
+def cell_to_pos_00(cell):
+	return cell_to_pos_offset(cell, (0, 0))
+
+def cell_to_pos(cell):
+	return cell_to_pos_offset(cell, (0.5, 0.5))
+
+def pos_to_cell(pos):
+	return (pos[0] // CELL_W, pos[1] // CELL_H)
+
+def cell_distance(cell1, cell2):
+	return abs(cell2[0] - cell1[0]) + abs(cell2[1] - cell1[1])
+
+def sort_cells(cells):
+	return sorted(cells, key=lambda cell: (cell[1], cell[0]))
+
+class Area:
+	x1 = None
+	y1 = None
+	x2 = None
+	y2 = None
+	size_x = None
+	size_y = None
+	x_range = None
+	y_range = None
+
+	@property
+	def num_cells(self):
+		return len(self.x_range) * len(self.y_range)
+
+	@property
+	def cells(self):
+		return product(self.x_range, self.y_range)
+
+	@property
+	def cell11(self):
+		return (self.x1, self.y1)
+
+	@property
+	def cell12(self):
+		return (self.x1, self.y2)
+
+	@property
+	def cell21(self):
+		return (self.x2, self.y1)
+
+	@property
+	def cell22(self):
+		return (self.x2, self.y2)
+
+	def is_cell_evnevn(self, cell):
+		return (cell[0] - self.x1) % 2 == 0 and (cell[1] - self.y1) % 2 == 0
+
+	def is_cell_evnodd(self, cell):
+		return (cell[0] - self.x1) % 2 == 0 and (cell[1] - self.y1) % 2 == 1
+
+	def is_cell_oddevn(self, cell):
+		return (cell[0] - self.x1) % 2 == 1 and (cell[1] - self.y1) % 2 == 0
+
+	def is_cell_oddodd(self, cell):
+		return (cell[0] - self.x1) % 2 == 1 and (cell[1] - self.y1) % 2 == 1
+
+	def is_cell_inside(self, cell, margin=0):
+		return self.x1 + margin <= cell[0] <= self.x2 - margin and self.y1 + margin <= cell[1] <= self.y2 - margin
+
+	def is_cell_on_margin(self, cell, margin=0):
+		if margin > 0:
+			return self.is_cell_inside(cell) and not self.is_cell_inside(cell, margin)
+		if margin < 0:
+			return not self.is_cell_inside(cell) and self.is_cell_inside(cell, margin)
+		return False
+

cursor.py
@@ -1,5 +1,6 @@
 from objects import char, lifts
-from cellactor import CellActor, apply_diff, get_actor_on_cell
+from cellactor import CellActor, get_actor_on_cell
+from celltools import apply_diff
 
 class Cursor(CellActor):
 	def __init__(self, image=None):

draw.py
@@ -1,7 +1,7 @@
 import pygame
 from translate import _
 from game import game
-from cellactor import apply_diff
+from celltools import apply_diff
 from sizetools import import_size_constants
 
 def draw_central_flash(full=False, color=(0, 40, 40)):

image.py
@@ -2,7 +2,7 @@ import pygame
 import pgzero
 from constants import DATA_DIR
 from sizetools import CELL_W, CELL_H
-from cellactor import *
+from celltools import *
 
 __all__ = [
 	'load_image',

room.py
@@ -1,4 +1,4 @@
-from cellactor import Area, cell_distance
+from celltools import Area, cell_distance
 from flags import flags
 
 # TODO: Consider to move all room related things from flags to here

solution.py
@@ -3,7 +3,7 @@ from flags import flags
 from config import SOLUTION_MOVE_DELAY, SOLUTION_MOVE_DELAY_RANGE, SOLUTION_MOVE_DELAY_CHANGE
 from objects import char
 from constants import DIR_NAMES, DIRS_BY_NAME
-from cellactor import apply_diff, cell_diff
+from celltools import apply_diff, cell_diff
 from clipboard import clipboard
 from statusmessage import set_status_message
 

commit c08b3e7cc56c1887fcb2b992bc8810da8ca8a724

Fix mirror flip drawing by adding missing import
Missed this case on celltools refactoring.

Fixes: 034be22f94c1 ("Move all cell related functions and Area from cellactor to new celltools")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

mirror.py
@@ -1,6 +1,7 @@
 import pygame
 from typing import Union
 from cellactor import CellActor
+from celltools import apply_diff
 from common import die
 from game import game
 from constants import MIRROR_INACTIVE_OPACITY, MIRROR_ORIENTATION_CHARS