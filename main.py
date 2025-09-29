mig0/dungeon

commit 6712192ee55ab4f60fc54b1fa74878de1dae494b

Add convert_outer_floors to be used with loaded barrel maps
In barrel puzzle, we generate the random puzzle with initially all walls,
so in on_generate_map() we want to call convert_inner_walls().

And the map files are all floor instead, so in on_load_map() we want to
call convert_outer_floors().

The effect is the same: the outer (inaccessible) area is converted either
to CELL_VOID (if bg_image is given) or to CELL_OUTER_WALL.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -283,6 +283,18 @@ def convert_outer_walls(cell_type=None):
 	if cell_type is not None:
 		replace_outer_walls(cell_type)
 
+def convert_outer_floors(cell_type=None):
+	floor_cells_to_convert = set()
+	for cy in (0, MAP_SIZE_Y - 1):
+		for cx in (0, MAP_SIZE_X - 1):
+			if map[cx, cy] in CELL_FLOOR_TYPES:
+				floor_cells_to_convert.update(get_accessible_cells((cx, cy)))
+	for cell in floor_cells_to_convert:
+		map[cell] = CELL_OUTER_WALL
+
+	if cell_type is not None:
+		replace_outer_walls(cell_type)
+
 def get_theme_image_name(image_name):
 	for full_image_name in (theme_prefix + image_name, DEFAULT_IMAGE_PREFIX + image_name):
 		if os.path.isfile(IMAGES_DIR_PREFIX + full_image_name + '.png'):
@@ -741,6 +753,7 @@ class Globals:
 	debug_map = debug_map
 	is_cell_in_map = is_cell_in_map
 	convert_outer_walls = convert_outer_walls
+	convert_outer_floors = convert_outer_floors
 	load_image = load_image
 	load_theme_cell_image = load_theme_cell_image
 	colorize_cell_image = colorize_cell_image
@@ -828,6 +841,7 @@ def generate_map():
 
 	if "map_file" in level or "map_string" in level:
 		if ret := load_map(level.get("map_file") or io.StringIO(level["map_string"])):
+			set_room(0)
 			puzzle.on_create_map(map)
 			puzzle.on_load_map(*ret)
 			return

puzzle/__init__.py
@@ -31,7 +31,7 @@ class Puzzle:
 		return True
 
 	def has_border(self):
-		return self.level.get("has_border", True)
+		return self.level.get("has_border", True) and self.level.get("map_file") is None and self.level.get("map_string") is None
 
 	def is_long_generation(self):
 		return False

puzzle/barrel.py
@@ -175,6 +175,10 @@ class BarrelPuzzle(Puzzle):
 
 		return False
 
+	def on_load_map(self, special_cell_values, extra_values):
+		self.Globals.convert_outer_floors(CELL_VOID if "bg_image" in self.level else None)
+		self.on_generate_map()
+
 	def on_generate_map(self):
 		self.Globals.convert_outer_walls(CELL_VOID if "bg_image" in self.level else None)
 

mig0/puzzle-dungeon

commit 705935988f1db11b56c70ec51775a68b32662fde

Fix convert_outer_floors
It worked only for floors at map corners. Now it works for floors
anywhere on the border.

Also use cell var in the related functions for readability.

Fixes: 6712192ee55a ("Add convert_outer_floors to be used with loaded barrel maps")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -190,24 +190,29 @@ def is_outer_wall(cell, void_is_like_wall=False):
 def replace_outer_walls(*cell_types):
 	for cy in MAP_Y_RANGE:
 		for cx in MAP_X_RANGE:
-			if game.map[cx, cy] == CELL_OUTER_WALL:
-				game.map[cx, cy] = choice(cell_types)
+			cell = cx, cy
+			if game.map[cell] == CELL_OUTER_WALL:
+				game.map[cell] = choice(cell_types)
 
 def convert_outer_walls(cell_type=None, void_is_like_wall=False):
 	for cy in MAP_Y_RANGE:
 		for cx in MAP_X_RANGE:
-			if is_outer_wall((cx, cy), void_is_like_wall=void_is_like_wall):
-				game.map[cx, cy] = CELL_OUTER_WALL
+			cell = cx, cy
+			if is_outer_wall(cell, void_is_like_wall=void_is_like_wall):
+				game.map[cell] = CELL_OUTER_WALL
 
 	if cell_type is not None:
 		replace_outer_walls(*cell_type)
 
 def convert_outer_floors(cell_type=None):
 	floor_cells_to_convert = set()
-	for cy in (0, MAP_SIZE_Y - 1):
-		for cx in (0, MAP_SIZE_X - 1):
-			if game.map[cx, cy] in CELL_FLOOR_TYPES:
-				floor_cells_to_convert.update(get_accessible_cells((cx, cy)))
+	for cy in MAP_Y_RANGE:
+		for cx in MAP_X_RANGE:
+			if not (cx == 0 or cy == 0 or cx == MAP_SIZE_X - 1 or cy == MAP_SIZE_Y - 1):
+				continue
+			cell = cx, cy
+			if game.map[cell] in CELL_FLOOR_TYPES and not cell in floor_cells_to_convert:
+				floor_cells_to_convert.update(get_accessible_cells((cell)))
 	for cell in floor_cells_to_convert:
 		game.map[cell] = CELL_OUTER_WALL
 