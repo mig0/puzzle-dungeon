mig0/puzzle-dungeon

commit f880be3d9651674a8dba6f3d1a44608652411a16

Support special argument type letslogic:<ll-collection-id> for playing
This is somewhat similar to clipboard: argument, but instead of getting
sokoban levels from clipboard, fetch them from LetsLogic web site.

The loaded ll collections (and their levels) are cached locally, so there
will be no need to fetch them again.

Try:
	dungeon letslogic:999

All kinds of errors are hundled properly. It does not work without
internet obviously, and can be stuck if the web site is firewalled.

Implement the letslogic API and use puzzle-dungeon user for this.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

load.py
@@ -1,7 +1,10 @@
 import io
 import os
 import sys
+import codecs
+from urllib.request import urlopen
 from debug import debug
+from common import warn
 from constants import *
 
 USER_DIR = None
@@ -272,3 +275,58 @@ def load_map(filename_or_stringio, special_cell_types={}):
 
 	return (special_cell_values, extra_values)
 
+def fetch_letslogic(action):
+	url = LETSLOGIC_API_URL + action
+	debug(2, "Fetching %s" % url)
+	key = LETSLOGIC_API_KEY
+	if key.startswith('_'):
+		key = codecs.decode(key[1:][::-1], 'rot13')
+	try:
+		with urlopen(url, data=bytes('key=' + key, 'utf-8')) as res:
+			if res.status != 200:
+				warn("Got HTTP status %d on %s" % (res.status, url))
+				return None
+			output = res.read().decode()
+			debug(3, output)
+			return output
+	except Exception as e:
+		warn("%s on %s" % (e, url))
+		return None
+
+def get_ll_sokoban_level_string(config, ll_coll_name):
+	map_str = "\n".join([config['map'][i:i+config['width']] for i in range(0, len(config['map']), config['width'])])
+	string = map_str.translate(dict((ord(cn), (ch)) for cn, ch in zip('01234567', '-#@$.*+_'))) + "\n"
+	string += "ID: %s\n" % config['id']
+	string += "Title: %s\n" % config['title']
+	string += "Collection: %s\n" % ll_coll_name
+	string += "Author: %s\n" % config['author']
+	return string
+
+def fetch_letslogic_collection(ll_coll_id):
+	if not str(ll_coll_id).isascii() or not str(ll_coll_id).isdigit():
+		warn("Numeric parameter required to fetch letslogic collection")
+		return None
+	ll_coll_filename = "maps/sokoban/letslogic/%s.txt" % ll_coll_id
+	if exists_user_file(ll_coll_filename):
+		return load_user_file(ll_coll_filename)
+
+	level_configs = None
+	output = fetch_letslogic("collection/%s" % ll_coll_id)
+
+	if output is not None:
+		try:
+			ll_level_configs = eval(output)
+		except:
+			warn("Failed to parse letslogic response\n" + output)
+			return None
+		sokoban_coll_string = ""
+		for ll_level_config in ll_level_configs:
+			sokoban_level_string = get_ll_sokoban_level_string(ll_level_config, ll_coll_id)
+			ll_level_filename = "maps/sokoban/letslogic/levels/%s.txt" % ll_level_config['id']
+			save_user_file(ll_level_filename, sokoban_level_string)
+			sokoban_coll_string += sokoban_level_string + "\n"
+		save_user_file(ll_coll_filename, sokoban_coll_string)
+	else:
+		warn("Can't fetch letslogic collection, check url, key or internet")
+
+	return sokoban_coll_string

config.py
@@ -120,3 +120,6 @@ pgconsole_config = {
 		"display_lines": 20,
 	}
 }
+
+LETSLOGIC_API_URL = 'https://letslogic.com/api/v1/'
+LETSLOGIC_API_KEY = '_n2p9882po92s78rp6rnq9rsnn8q5q6r4p797245qo375s105psq38n19rs03sqo0'

main.py
@@ -23,7 +23,7 @@ from level import Collection
 from cmdargs import cmdargs
 from game import game
 from drop import draw_status_drops
-from load import load_map, parse_map_file_signature
+from load import *
 from flags import flags
 from puzzle import create_puzzle, Puzzle
 from solution import solution, set_solution_funcs
@@ -1987,6 +1987,9 @@ def handle_cmdargs():
 					level_configs.extend(parse_sokoban_levels(map_string))
 				else:
 					warn("Ignoring 'clipboard:', since clipboard is empty")
+			elif arg.startswith("letslogic:"):
+				if map_string := fetch_letslogic_collection(arg[10:]):
+					level_configs.extend(parse_sokoban_levels(map_string))
 			else:
 				warn("Ignoring unknown argument %s" % arg)
 		if level_configs:

commit cea6c09288e29091525d177d70bfb2d3622ad563

load: Fix parsing letslogic collection response with null
Fixes: f880be3d9651 ("Support special argument type letslogic:<ll-collection-id> for playing")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

load.py
@@ -343,10 +343,11 @@ def fetch_letslogic_collection(ll_coll_id):
 	ll_coll_title = ll_collections.get(ll_coll_id, {'title': ll_coll_id})['title']
 
 	if output is not None:
+		null = None
 		try:
 			ll_level_configs = eval(output)
 		except:
-			warn("Failed to parse letslogic response\n" + output)
+			warn("Failed to parse letslogic collection response\n" + output)
 			return None
 		sokoban_coll_string = ""
 		for ll_level_config in ll_level_configs: