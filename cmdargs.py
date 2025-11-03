87562e44bd95ea857a87c82cf03c7bbb2484604e
Mikhael Goikhman <migo@freeshell.org>
2025-11-03 22:35:24 +0200

    cmdargs: Implement --run-profiler (-p)
    When give, --run-profiler or -p starts profiler and stops it on exit.
    
    Rename previous -p (--puzzle-type) to -P to avoid conflict.
    
    Also, reorder some options, move at the top the ones that are common with
    the upcoming sokodun script.
    
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- cmdargs.py
+++ cmdargs.py
@@ -1,22 +1,25 @@
 import os
 import sys
 import shlex
+import atexit
 import argparse
 from debug import debug
+from profiler import profiler
 
 sys.argv[0] = 'dungeon'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', metavar='SELECTOR', action='append', default=[], help='debug level or feature (repeatable)')
-parser.add_argument('-s', '--start', metavar='LEVEL-ID', type=str, help="start with given level or collection id")
 parser.add_argument('-C', '--list-collections', help="list all collections", action='store_true')
 parser.add_argument('-L', '--list-ll-collections', help="list all letslogic collections", action='store_true')
 parser.add_argument('-n', '--use-numeric', help="use numeric ids to list collections", action='store_true')
+parser.add_argument('-r', '--reverse-barrel-mode', '--reverse', help="apply reverse barrel mode to custom levels", action='store_true')
+parser.add_argument('-p', '--run-profiler', help="run code with profiler", action='store_true')
+parser.add_argument('-s', '--start', metavar='LEVEL-ID', type=str, help="start with given level or collection id")
 parser.add_argument('-b', '--bg-image', type=str, help="use this bg image for custom levels")
 parser.add_argument('-c', '--cloud-mode', help="apply cloud mode to custom levels", action='store_true')
 parser.add_argument('-m', '--music', type=str, help="use this music for custom levels")
-parser.add_argument('-p', '--puzzle-type', '--puzzle', type=str, help="use this puzzle type for custom levels")
-parser.add_argument('-r', '--reverse-barrel-mode', '--reverse', help="apply reverse barrel mode to custom levels", action='store_true')
+parser.add_argument('-P', '--puzzle-type', '--puzzle', type=str, help="use this puzzle type for custom levels")
 parser.add_argument('-t', '--theme', type=str, help="use this theme for custom levels")
 parser.add_argument('-M', '--no-music', help="turn music off", action='store_true')
 parser.add_argument('-S', '--no-sound', help="turn sound off", action='store_true')
@@ -30,3 +33,7 @@ if cmdargs_str == "''":
 cmdargs = parser.parse_args(shlex.split(cmdargs_str))
 
 debug.configure(cmdargs.debug)
+
+if cmdargs.run_profiler:
+	profiler.start()
+	atexit.register(lambda: profiler.stop())
