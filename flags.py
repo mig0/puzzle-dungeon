mig0/puzzle-dungeon

commit bc58a2bde391a739224bec4599ae71c7d4c2a154

Correctly implement default-map-size and screen-size-fitting-display
Now DEFAULT_MAP_SIZE (was: 13x13) is set from the actual display size.
On resolutions 1366x768 it may be 15x9. On HDD it is still 13x13.

Both Ctrl-S (fit screen content to display) and the default-map-size try
to fit the visible screen into (DISPLAY_W, DISPLAY_H - EXTRA_DISPLAY_H).
Currently EXTRA_DISPLAY_H is 71 pixels that should be all right for panel
and window title in the current GNOME and Windows 11.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

level.py
@@ -2,7 +2,7 @@ import os
 from copy import deepcopy
 from common import warn, die
 from config import DEFAULT_NUM_ENEMIES
-from sizetools import DEFAULT_MAP_SIZE
+from sizetools import get_default_map_size
 from cmdargs import cmdargs
 
 special_config_keys = ('bg-image', 'cloud-mode', 'music', 'puzzle-type', 'theme')
@@ -42,7 +42,7 @@ class Level:
 		self.has_finish = False
 		self.has_start = False
 		self.map_file = None
-		self.map_size = DEFAULT_MAP_SIZE
+		self.map_size = get_default_map_size()
 		self.map_string = None
 		self.music = collection.config.get('music') or "valiant_warriors"
 		self.name = None

sizetools.py
@@ -3,6 +3,10 @@ import sys, inspect
 CELL_W = 64
 CELL_H = 64
 
+DISPLAY_W = None
+DISPLAY_H = None
+EXTRA_DISPLAY_H = 71
+
 MAP_POS_X1 = 0
 MAP_POS_Y1 = 0
 
@@ -40,6 +44,20 @@ ROOM_9_Y1 = None
 ROOM_9_Y2 = None
 ROOM_9_BORDERS_X = None
 ROOM_9_BORDERS_Y = None
+
+def set_display_size(display_size):
+	global DISPLAY_W, DISPLAY_H, DEFAULT_MAP_SIZE
+	DISPLAY_W, DISPLAY_H = display_size
+	map_size_y = min(13, (DISPLAY_H - EXTRA_DISPLAY_H) // CELL_H - 1)
+	map_size_x = 13 + ((11 - ((map_size_y + 1) // 2 * 2 - 1)) if map_size_y < 11 else 0)
+	assert 5 <= map_size_y <= 13 and map_size_x * CELL_W <= DISPLAY_W, f"Can't fit default map into display {display_size}"
+	DEFAULT_MAP_SIZE = (map_size_x, map_size_y)
+
+def get_display_size():
+	return (DISPLAY_W, DISPLAY_H)
+
+def get_default_map_size():
+	return DEFAULT_MAP_SIZE
 
 def set_map_size(size, has_border=True):
 	global MAP_SIZE

sokobanparser.py
@@ -1,6 +1,7 @@
 import io
 import os
 from constants import *
+from sizetools import get_default_map_size
 from random import randint
 from common import die, open_read
 
@@ -96,8 +97,7 @@ def create_map_string(lines):
 	real_size_x = len(lines[0])
 
 	# min size
-	min_size_x = 13
-	min_size_y = 13
+	min_size_x, min_size_y = get_default_map_size()
 
 	# calculate intended size_x, size_y
 	size_y = max(min_size_y, real_size_y)

game.py
@@ -2,6 +2,7 @@ from common import load_tabbed_yaml, get_pgzero_game_from_stack, warn, die
 from config import DATA_DIR, pgconsole_config
 from level import Collection, Level, parse_level_id
 from sokobanparser import parse_sokoban_levels
+from sizetools import set_display_size
 from room import room
 import pygame
 import copy
@@ -105,6 +106,9 @@ class Game:
 		self.in_level = False
 		self.during_undo = False
 		self.last_char_move = None
+
+		self._set_display_size()
+		self.screen_size_fitting_display = None
 
 		self.collections = []
 		self.level = Level()
@@ -239,6 +243,17 @@ class Game:
 		self.during_undo = False
 
 		return True
+
+	def _set_display_size(self):
+		display_size = pygame.display.get_desktop_sizes()[0]
+		set_display_size(display_size)
+
+	def calc_screen_size_fitting_display(self):
+		if DISPLAY_W > WIDTH and DISPLAY_H - EXTRA_DISPLAY_H > HEIGHT:
+			self.screen_size_fitting_display = None
+			return
+		scale_factor = max(WIDTH / DISPLAY_W, HEIGHT / (abs(DISPLAY_H - EXTRA_DISPLAY_H) or 1))
+		self.screen_size_fitting_display = (int(WIDTH / scale_factor), int(HEIGHT / scale_factor))
 
 	def set_requested_new_level(self, level_id=None, reload_stored=False):
 		if not level_id:

main.py
@@ -34,9 +34,6 @@ from mainscreen import main_screen_level
 from sokobanparser import parse_sokoban_levels
 from statusmessage import reset_status_messages, set_status_message, set_quick_status_message, draw_status_message
 
-DISPLAY_WIDTH, DISPLAY_HEIGHT = pygame.display.get_desktop_sizes()[0]
-
-display_size_to_fit = None
 scale_to_display = False
 
 # set data dir and default encoding for the whole program
@@ -1099,13 +1096,7 @@ def init_new_level(level_id, reload_stored=False):
 	draw_apply_sizes()
 	flags.apply_sizes()
 
-	global display_size_to_fit
-	if not DISPLAY_WIDTH or not DISPLAY_HEIGHT or DISPLAY_WIDTH > WIDTH and DISPLAY_HEIGHT > HEIGHT:
-		display_size_to_fit = None
-	else:
-		scale_factor = max(WIDTH / DISPLAY_WIDTH, HEIGHT / (abs(DISPLAY_HEIGHT - 68) or 1))
-		display_size_to_fit = (int(WIDTH / scale_factor), int(HEIGHT / scale_factor))
-
+	game.calc_screen_size_fitting_display()
 	game.init_console()
 
 	bg_image = None
@@ -1335,8 +1326,8 @@ def draw():
 			draw_central_flash()
 		screen.draw.text(goal_line, center=(POS_CENTER_X, POS_CENTER_Y), color='#FFFFFF', gcolor="#66AA00", owidth=1.2, ocolor="#404030", alpha=1, fontsize=40)
 
-	if scale_to_display and display_size_to_fit:
-		scaled = pygame.transform.smoothscale(screen.surface, display_size_to_fit)
+	if scale_to_display and game.screen_size_fitting_display:
+		scaled = pygame.transform.smoothscale(screen.surface, game.screen_size_fitting_display)
 		screen.fill((0, 0, 0))
 		screen.blit(scaled, (0, 0))
 

commit de14e6aab96fed29f9baabb010b4baf6172d5196

Fix fitting map size for levels with multi-room and loaded maps
The pre-created maps now correctly use the map size and not
fitting_map_size.

When no map size is specified (i.e. in level config map-size or in loaded
map), still use map size 13x13 for multi-rooms. For non multi-room levels
use fitting map size.

Fixes: bc58a2bde391 ("Correctly implement default-map-size and screen-size-fitting-display")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

flags.py
@@ -1,5 +1,7 @@
 from constants import *
-from sizetools import import_size_constants
+from sizetools import DEFAULT_MAP_SIZE, get_fitting_map_size, import_size_constants
+from common import die
+from load import detect_map_file
 
 class Flags:
 	def parse_level(self, level):
@@ -26,6 +28,18 @@ class Flags:
 			self.NUM_ROOMS = 1
 
 		self.MULTI_ROOMS = self.NUM_ROOMS > 1
+
+		if level.map_size is None:
+			if level.map_string or level.map_file:
+				map_info = detect_map_file(level.map_file, map_string=level.map_string)
+				if map_info is None:
+					die("Internal error: Can't detect map file")
+				is_sokoban_map, error, puzzle_type, size = map_info
+				if not size:
+					die("Internal error: Can't detect map file size")
+				level.map_size = size
+		if level.map_size is None:
+			level.map_size = DEFAULT_MAP_SIZE if self.MULTI_ROOMS else get_fitting_map_size()
 
 	def apply_sizes(self):
 		import_size_constants()

level.py
@@ -2,7 +2,6 @@ import os
 from copy import deepcopy
 from common import warn, die
 from config import DEFAULT_NUM_ENEMIES
-from sizetools import get_default_map_size
 from cmdargs import cmdargs
 
 special_config_keys = ('bg-image', 'cloud-mode', 'music', 'puzzle-type', 'theme')
@@ -42,7 +41,7 @@ class Level:
 		self.has_finish = False
 		self.has_start = False
 		self.map_file = None
-		self.map_size = get_default_map_size()
+		self.map_size = None
 		self.map_string = None
 		self.music = collection.config.get('music') or "valiant_warriors"
 		self.name = None
@@ -68,7 +67,7 @@ class Level:
 			if hasattr(self, field):
 				default = getattr(self, field)
 				expected_type = type(default) if default is not None or value is None else \
-					int if any(map(key.endswith, ("-power", "-limit", "-health"))) else str
+					tuple if key == 'map-size' else int if any(map(key.endswith, ("-power", "-limit", "-health"))) else str
 				if expected_type != type(value):
 					warn("Ignoring config %s=%s in level %s; expected type %s" %
 						(key, str(value), self.get_id(), expected_type.__name__))

sizetools.py
@@ -11,6 +11,7 @@ MAP_POS_X1 = 0
 MAP_POS_Y1 = 0
 
 DEFAULT_MAP_SIZE = (13, 13)
+FITTING_MAP_SIZE = None
 
 MAP_SIZE = None
 MAP_SIZE_X = None
@@ -46,18 +47,18 @@ ROOM_9_BORDERS_X = None
 ROOM_9_BORDERS_Y = None
 
 def set_display_size(display_size):
-	global DISPLAY_W, DISPLAY_H, DEFAULT_MAP_SIZE
+	global DISPLAY_W, DISPLAY_H, FITTING_MAP_SIZE
 	DISPLAY_W, DISPLAY_H = display_size
-	map_size_y = min(13, (DISPLAY_H - EXTRA_DISPLAY_H) // CELL_H - 1)
+	map_size_y = min(13, ((DISPLAY_H - EXTRA_DISPLAY_H) // CELL_H) // 2 * 2 - 1)
 	map_size_x = 13 + ((11 - ((map_size_y + 1) // 2 * 2 - 1)) if map_size_y < 11 else 0)
 	assert 5 <= map_size_y <= 13 and map_size_x * CELL_W <= DISPLAY_W, f"Can't fit default map into display {display_size}"
-	DEFAULT_MAP_SIZE = (map_size_x, map_size_y)
+	FITTING_MAP_SIZE = (map_size_x, map_size_y)
 
 def get_display_size():
 	return (DISPLAY_W, DISPLAY_H)
 
-def get_default_map_size():
-	return DEFAULT_MAP_SIZE
+def get_fitting_map_size():
+	return FITTING_MAP_SIZE
 
 def set_map_size(size, has_border=True):
 	global MAP_SIZE

sokobanparser.py
@@ -1,7 +1,7 @@
 import io
 import os
 from constants import *
-from sizetools import get_default_map_size
+from sizetools import get_fitting_map_size
 from random import randint
 from common import die, open_read
 
@@ -97,7 +97,7 @@ def create_map_string(lines):
 	real_size_x = len(lines[0])
 
 	# min size
-	min_size_x, min_size_y = get_default_map_size()
+	min_size_x, min_size_y = get_fitting_map_size()
 
 	# calculate intended size_x, size_y
 	size_y = max(min_size_y, real_size_y)