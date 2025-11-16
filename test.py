32b2e3e7c47cdab575fa99ed3c117ba07c20c93d
Mikhael Goikhman <migo@freeshell.org>
2025-11-16 21:51:44 +0200

    test: Enhance TestSuite with new features and add suppress_output
    Add wrapper function suppress_output that is used in TestSuite and may be
    used externally. Usage:
    	with suppress_output():
    		do_something_that_writes_to_stdout_or_stderr
    
    Add new methods to TestSuite:
    	test.catch(callable_cond, optional_descr)
    	test.no_catch(callable_cond, optional_descr)
    
    They check for exceptions thrown.
    
    Also test.ok() now accepts callable_cond instead of cond.
    
    When callable_cond is called in all 3 methods, this is done under
    suppress_output(), and SystemExit as well as regular exceptions are
    caught, with informative messages produced.
    
    Also, now test.ok() can be called without error. Just like with catch and
    no_catch, if the second argument is missing, then the actual code inside
    calling script extracted if possible and is used for descr or error. I.e.
    if this is failed:
    
    	ok(obj.something())
    
    then this error is produced:
    
    	10 - FAIL: obj.something()
    
    Also, now constructor parameters assert_on_fail, verbose_on_pass,
    run_profiler default to None. If not explicitely given, then script args
    -a, -v and -p are probed respectivelly as the default.
    
    Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

--- test.py
+++ test.py
@@ -1,14 +1,32 @@
+import contextlib
 import inspect
+import sys
+import io
 from profiler import profiler
 
+@contextlib.contextmanager
+def suppress_output():
+	_saved_out = sys.stdout
+	_saved_err = sys.stderr
+	sys.stdout = io.StringIO()
+	sys.stderr = io.StringIO()
+	try:
+		yield
+	finally:
+		sys.stdout = _saved_out
+		sys.stderr = _saved_err
+
 class TestSuite:
-	def __init__(self, name, assert_on_fail=False, verbose_on_pass=False, run_profiler=False):
+	def __init__(self, name, assert_on_fail=None, verbose_on_pass=None, run_profiler=None):
+		# default to the command line flags, but keep explicit values
+		args = sys.argv[1:]
+
 		self.reset()
 		self.name = name
 		self.last_function = None
-		self.assert_on_fail = assert_on_fail
-		self.verbose_on_pass = verbose_on_pass
-		self.run_profiler = run_profiler
+		self.assert_on_fail  = '-a' in args if assert_on_fail  is None else assert_on_fail
+		self.verbose_on_pass = '-v' in args if verbose_on_pass is None else verbose_on_pass
+		self.run_profiler    = '-p' in args if run_profiler    is None else run_profiler
 		if self.run_profiler:
 			profiler.start()
 
@@ -22,20 +40,38 @@ class TestSuite:
 			print("# %s" % function)
 			self.last_function = function
 
-	def ok(self, is_passed, error):
+	def ok(self, cond, error=None):
 		if self.verbose_on_pass:
 			self.print_function_once()
+
 		self.num_total += 1
-		if is_passed:
+		if error is None:
+			error = self._extract_cond_code_str()
+		if callable(cond):
+			cond, error = self._call(cond, error)
+
+		if cond:
 			self.num_passed += 1
 			if self.verbose_on_pass:
-				print("%d - PASS" % (self.num_total))
+				print("%d - PASS" % self.num_total)
 		else:
 			if not self.verbose_on_pass:
 				self.print_function_once()
 			print("%d - FAIL: %s" % (self.num_total, error))
 			if self.assert_on_fail:
 				assert False, error
+
+	def catch(self, cond, descr=None):
+		if descr is None:
+			descr = self._extract_cond_code_str()
+		_, exc_str = self._call(cond)
+		self.ok(exc_str, "Expected to catch exception in: %s" % descr)
+
+	def no_catch(self, cond, descr=None):
+		if descr is None:
+			descr = self._extract_cond_code_str()
+		_, exc_str = self._call(cond)
+		self.ok(not exc_str, "Unexpected %s was caught in: %s" % (exc_str, descr))
 
 	def eq(self, value1, value2):
 		self.ok(value1 == value2, "Got %s, but expected %s" % (str(value1), str(value2)))
@@ -59,7 +95,7 @@ class TestSuite:
 		self.ok(type(cell) == tuple and len(cell) == 2 and all(type(i) == int for i in cell), "Got %s, but expected cell" % str(cell))
 
 	def is_in(self, elem, mapping):
-		self.ok(elem in mapping, "Elem %s not is in %s, unexpected" % (str(elem), type(mapping).__name__))
+		self.ok(elem in mapping, "Elem %s is not in %s, unexpected" % (str(elem), type(mapping).__name__))
 
 	def not_in(self, elem, mapping):
 		self.ok(elem not in mapping, "Elem %s is in %s, unexpected" % (str(elem), type(mapping).__name__))
@@ -96,3 +132,47 @@ class TestSuite:
 		self.reset()
 		if self.run_profiler:
 			profiler.stop()
+
+	def _call(self, callable_cond, error=""):
+		assert callable(callable_cond)
+		exc_str = None
+		cond = None
+		with suppress_output():
+			try:
+				cond = callable_cond()
+			except SystemExit as e:
+				cond = False
+				exc_str = "SystemExit"
+			except Exception as e:
+				cond = False
+				exc_str = f"{e.__class__.__name__} '{e}'"
+		if error and exc_str:
+			error = f"{error}; also {exc_str} is thrown"
+		return cond, error or exc_str
+
+	def _extract_cond_code_str(self):
+		frame = inspect.currentframe().f_back.f_back
+		filename = frame.f_code.co_filename
+		lineno = frame.f_lineno
+
+		code_str = "Condition in file %s line %d" % (filename, lineno)
+		try:
+			import linecache
+			line = linecache.getline(filename, lineno).strip()
+			# expect something like: test.ok(something), match parentheses
+			idx = line.find('(')
+			if idx >= 0:
+				code_str = line[idx + len('('):]
+				par, end = 1, 0
+				for i, ch in enumerate(code_str):
+					if ch == '(':
+						par += 1
+					elif ch == ')':
+						par -= 1
+						if par == 0:
+							end = i
+							break
+				code_str = code_str[:end].strip()
+		except Exception:
+			pass
+		return code_str
