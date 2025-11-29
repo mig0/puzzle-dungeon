mig0/puzzle-dungeon

commit 72a87331f64bfd4068d0dafd6bef4fa0d4d2f852

sokobansolver: Do not call find_path during solution search, only after
Call get_accessible_distance instead of find_path (that is significantly
faster) during solution search. Call find_path only when reconstructing
the best found solution. This may save about 30% of time.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

sokobansolver.py
@@ -146,8 +146,8 @@ class Position:
 	def segments_str(self):
 		if self._segments_str is None:
 			segment_strs = []
-			for path_cells, char_cell, barrel_cell in self.segments:
-				segment_strs.append("+%d %s -> %s" % (len(path_cells), char_cell, barrel_cell))
+			for distance, char_cell, barrel_cell in self.segments:
+				segment_strs.append("+%d %s -> %s" % (distance, char_cell, barrel_cell))
 			self._segments_str = ' '.join(segment_strs) or 'root'
 		return self._segments_str
 
@@ -159,7 +159,9 @@ class Position:
 
 	def to_solution_pairs(self):
 		solution_pairs = self.parent.to_solution_pairs() if self.parent else []
-		for path_cells, char_cell, barrel_cell in self.segments:
+		for _, char_cell, barrel_cell in self.segments:
+			path_cells = grid.find_path(self.parent.char_cell, char_cell, self.parent.super.barrel_cells)
+			assert path_cells is not None, "Bug: Failed to reconstruct char path in solution"
 			solution_pairs.append([path_cells, DIR_NAMES[cell_diff(char_cell, barrel_cell, grid.reverse_barrel_mode, True)]])
 		return solution_pairs
 
@@ -328,7 +330,7 @@ class SokobanSolver():
 					new_char_cell, new_barrel_cell = apply_diff(char_cell, cell_diff(barrel_cell, char_cell)), char_cell
 				else:
 					new_char_cell, new_barrel_cell = barrel_cell, apply_diff(barrel_cell, cell_diff(char_cell, barrel_cell))
-				proto_segments.append(([], new_char_cell, new_barrel_cell))
+				proto_segments.append((0, new_char_cell, new_barrel_cell))
 
 		super_position = SuperPosition(barrel_cells, all_proto_segments)
 		self.visited_super_positions[super_position_id] = super_position
@@ -340,9 +342,9 @@ class SokobanSolver():
 	def create_child_position_or_reparent_if_better(self, position, segments):
 		num_moves, num_shifts = 0, 0
 		grid.set_barrels(position.super.barrel_cells)
-		for path_cells, char_cell, barrel_cell in segments:
+		for distance, char_cell, barrel_cell in segments:
 			new_char_cell, _ = grid.shift(char_cell, barrel_cell)
-			num_moves += len(path_cells) + 1
+			num_moves += distance + 1
 			num_shifts += 1
 		own_nums = num_moves, num_shifts
 
@@ -372,9 +374,9 @@ class SokobanSolver():
 		for proto_segments in position.super.all_proto_segments:
 			(_, char_cell, barrel_cell), *rest_segments = proto_segments
 			debug([position.depth], DBG_SOLV3, "Expanding %s -> %s" % (char_cell, barrel_cell))
-			char_path = grid.find_path(position.char_cell, char_cell, position.super.barrel_cells)
-			assert char_path is not None, "Bug in find_solution algorithm: no char path"
-			segments = [(char_path, char_cell, barrel_cell), *rest_segments]
+			distance = grid.get_accessible_distance(position.char_cell, char_cell, position.super.barrel_cells)
+			assert distance is not None, "Bug in find_solution algorithm: no char path"
+			segments = [(distance, char_cell, barrel_cell), *rest_segments]
 
 			self.create_child_position_or_reparent_if_better(position, segments)
 

commit b132d7ace7e900a16269a66931ac3d0a7089aaa4

sokobansolver: Fix solution for ZSB maps
In ZSB there are double pushes. Fix it generally for positions with
multiple segments (currently only ZSB maps). This is done by properly
performing barrel shifts before the next find_path.

Also, use Greedy algorithm by default for ZSB, since it finds solution
faster than A*.

Fixes: 72a87331f64b ("sokobansolver: Do not call find_path during solution search, only after")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

sokobansolver.py
@@ -157,11 +157,19 @@ class Position:
 
 	def to_solution_pairs(self):
 		solution_pairs = self.parent.to_solution_pairs() if self.parent else []
-		for _, char_idx, barrel_idx in self.segments:
-			path_cells = grid.find_path(self.parent.char_idx, char_idx, self.parent.super.barrel_idxs)
-			assert path_cells is not None, "Bug: Failed to reconstruct char path in solution"
-			char_cell, barrel_cell = grid.to_cells((char_idx, barrel_idx))
-			solution_pairs.append([path_cells, DIR_NAMES[cell_diff(char_cell, barrel_cell, grid.reverse_barrel_mode, True)]])
+		if self.parent:
+			grid.set_barrels(self.parent.super.barrel_idxs)
+			prev_char_cell = grid.to_cell(self.parent.char_idx)
+			for char_path_len, char_idx, barrel_idx in self.segments:
+				char_cell, barrel_cell = grid.to_cells((char_idx, barrel_idx))
+				path_cells = grid.find_path(prev_char_cell, char_cell, self.parent.super.barrel_idxs)
+				if path_cells is None:
+					grid.show_map(descr=f"{prev_char_cell} -> {char_cell}", char=char_idx, cell_colors={prev_char_cell: 1, barrel_cell: 31})
+				assert path_cells is not None, "Bug: Failed to reconstruct char path in solution"
+				assert len(path_cells) == char_path_len, f"Bug: Char path {path_cells} is not as expected (len {char_path_len})"
+				new_char_cell, new_barrel_cell = grid.shift(char_cell, barrel_cell)
+				solution_pairs.append([path_cells, DIR_NAMES[cell_diff(char_cell, barrel_cell, grid.reverse_barrel_mode, True)]])
+				prev_char_cell = new_char_cell
 		return solution_pairs
 
 	def __str__(self):
@@ -863,7 +871,7 @@ class SokobanSolver():
 		grid.set_barrels(barrel_cells)
 		grid.check_zsb()
 		if self.solution_alg is None:
-			self.solution_alg = SOLUTION_ALG_ASTAR if grid.is_zsb or self.return_first else SOLUTION_ALG_BFS
+			self.solution_alg = SOLUTION_ALG_GREED if grid.is_zsb or self.return_first else SOLUTION_ALG_BFS
 		self.char_cell = char_cell
 		self.barrel_cells = barrel_cells
 		global solver