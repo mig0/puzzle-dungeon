mig0/puzzle-dungeon

commit 6ddf4dbd9669a5738dae9d827c322e3e8e8b27ce

processcmdargs, level: Fix custom collection defaults in cmdargs
Fix Collection.with_level_config_defaults and call it with the correct
custom_collection to respect the command line args.

Make --cloud-mode and --reverse-barrel-mode to default to None not False,
not to override the level's definitions. If needed, in the future, flags
--no-cloud-mode and --no-reverse-barrel-mode could be added.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

level.py
@@ -129,7 +129,7 @@ class Collection:
 	def with_level_config_defaults(self, level_config):
 		level_config = deepcopy(level_config)
 		for key in ('bg-image', 'cloud-mode', 'music', 'puzzle-type', 'reverse-barrel-mode', 'theme'):
-			if level_config.get(key) is None and self.config.get(key) is not None:
+			if self.config.get(key) is not None:
 				level_config[key] = self.config[key]
 		return level_config
 

cmdargs.py
@@ -13,11 +13,11 @@ parser.add_argument('-d', '--debug', metavar='SELECTOR', action='append', defaul
 parser.add_argument('-C', '--list-collections', help="list all collections", action='store_true')
 parser.add_argument('-L', '--list-ll-collections', help="list all letslogic collections", action='store_true')
 parser.add_argument('-n', '--use-numeric', help="use numeric ids to list collections", action='store_true')
-parser.add_argument('-r', '--reverse-barrel-mode', '--reverse', help="apply reverse barrel mode to custom levels", action='store_true')
+parser.add_argument('-r', '--reverse-barrel-mode', '--reverse', help="apply reverse barrel mode to custom levels", action='store_true', default=None)
 parser.add_argument('-p', '--run-profiler', help="run code with profiler", action='store_true')
 parser.add_argument('-s', '--start', metavar='LEVEL-ID', type=str, help="start with given level or collection id")
 parser.add_argument('-b', '--bg-image', type=str, help="use this bg image for custom levels")
-parser.add_argument('-c', '--cloud-mode', help="apply cloud mode to custom levels", action='store_true')
+parser.add_argument('-c', '--cloud-mode', help="apply cloud mode to custom levels", action='store_true', default=None)
 parser.add_argument('-m', '--music', type=str, help="use this music for custom levels")
 parser.add_argument('-P', '--puzzle-type', '--puzzle', type=str, help="use this puzzle type for custom levels")
 parser.add_argument('-t', '--theme', type=str, help="use this theme for custom levels")

processcmdargs.py
@@ -51,10 +51,10 @@ def process_cmdargs(cmdargs, extra_custom_collection_config=None):
 			level_indexes.extend(range(int(arg[:sep_idx]), int(arg[sep_idx + 1:]) + 1))
 		elif is_valid_level_id(arg):
 			collection, _, level_config = get_collection_level_config_by_id(arg)
-			level_configs.append(collection.with_level_config_defaults(level_config))
+			level_configs.append(custom_collection.with_level_config_defaults(level_config))
 		elif collection := get_collection_by_id(arg):
 			for level_config in collection.level_configs:
-				level_configs.append(collection.with_level_config_defaults(level_config))
+				level_configs.append(custom_collection.with_level_config_defaults(level_config))
 		elif arg == "stdin:" or arg == "-":
 			level_configs.extend(parse_stdin_levels(arg, custom_collection.config) or [])
 		elif arg == "clipboard:":

sokodun
@@ -25,7 +25,7 @@ parser.add_argument('-d', '--debug', metavar='SELECTOR', action='append', defaul
 parser.add_argument('-C', '--list-collections', help="list all collections", action='store_true')
 parser.add_argument('-L', '--list-ll-collections', help="list all letslogic collections", action='store_true')
 parser.add_argument('-n', '--use-numeric', help="use numeric ids to list collections", action='store_true')
-parser.add_argument('-r', '--reverse-barrel-mode', '--reverse', help="apply reverse barrel mode to custom levels", action='store_true')
+parser.add_argument('-r', '--reverse-barrel-mode', '--reverse', help="apply reverse barrel mode to custom levels", action='store_true', default=None)
 parser.add_argument('-p', '--run-profiler', help="run code with profiler", action='store_true')
 parser.add_argument('-s', '--show-sok', help="show map in sok format rather than internal", action='store_true')
 parser.add_argument('-m', '--by-moves', help="find move optimal solution, not push/pull optimal", action='store_true')

commit 0795e885c1805a416403bd70d7b56c7dd4fd4dd2

level: Fix exporting collection levels into custom collection
When creating collection, first apply own collection defaults to all its
level configs. This also simplifies Level.set_from_config.

Then custom collection defaults (if any) will be applied to level configs
as intended.

Fixes: 6ddf4dbd9669 ("processcmdargs, level: Fix custom collection defaults in cmdargs")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

level.py
@@ -28,11 +28,11 @@ class Level:
 
 		# reset all fields to defaults
 		self.actors_always_revealed = False
-		self.bg_image = collection.config.get('bg-image') or None
+		self.bg_image = None
 		self.bg_image_crop = False
 		self.char_health = None
 		self.char_power = None
-		self.cloud_mode = collection.config.get('cloud-mode') or False
+		self.cloud_mode = False
 		self.disable_win = False
 		self.enemy_key_drop = False
 		self.four_rooms = False
@@ -43,26 +43,24 @@ class Level:
 		self.map_file = None
 		self.map_size = None
 		self.map_string = None
-		self.music = collection.config.get('music') or "valiant_warriors"
+		self.music = "valiant_warriors"
 		self.name = None
 		self.nine_rooms = False
 		self.num_enemies = DEFAULT_NUM_ENEMIES
 		self.grid_maze = False
 		self.random_maze = False
 		self.spiral_maze = False
-		self.puzzle_type = collection.config.get('puzzle-type') or 'Puzzle'
+		self.puzzle_type = 'Puzzle'
 		self.puzzle_config = {}
-		self.reverse_barrel_mode = collection.config.get('reverse-barrel-mode') or False
+		self.reverse_barrel_mode = False
 		self.stopless = False
-		self.theme = collection.config.get('theme') or "default"
+		self.theme = "default"
 		self.time_limit = None
 		self.title = None
 		self.use_clock = False
 
 		# apply config fields
 		for key, value in config.items():
-			if value is None and key in special_config_keys:
-				continue
 			field = key.replace("-", "_")
 			if hasattr(self, field):
 				default = getattr(self, field)
@@ -103,7 +101,9 @@ class Collection:
 		self.magic_n = config.get('magic-n', None)
 		self.n = config.get('n')
 
-		self.level_configs = config.get('levels', None)
+		level_configs = config.get('levels')
+		self.level_configs = tuple(self.with_level_config_defaults(level_config)
+			for level_config in level_configs) if level_configs else None
 
 	@property
 	def num_levels(self):