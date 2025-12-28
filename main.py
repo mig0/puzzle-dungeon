mig0/puzzle-dungeon

commit fd89f12a26ae697e1e2e572138d6090a00223e24

Add place flag to get_accessible_cells and get_accessible_neighbors
There is already place flag in is_accessible_cell that checks agains
CELL_CHAR_PLACE_OBSTACLES instead of CELL_CHAR_MOVE_OBSTACLES. Allow
to specify it in get_accessible_cells and get_accessible_neighbors.
This is needed at least for convert_outer_floors.

Also, add CELL_FINISH to CELL_CHAR_PLACE_OBSTACLES, since such cell types
as CELL_PLATE, CELL_PORTAL and CELL_GATE[12] are also here.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -224,7 +224,7 @@ def convert_outer_floors(cell_type=None):
 				continue
 			cell = cx, cy
 			if game.map[cell] in CELL_FLOOR_TYPES and not cell in floor_cells_to_convert:
-				floor_cells_to_convert.update(get_accessible_cells((cell)))
+				floor_cells_to_convert.update(get_accessible_cells(cell, place=True))
 	for cell in floor_cells_to_convert:
 		game.map[cell] = CELL_OUTER_WALL
 
@@ -328,7 +328,7 @@ def is_cell_accessible(cell, obstacles=None, place=False, allow_obstacles=False,
 		return False
 	return not is_cell_in_actors(cell, barrels if allow_enemy else barrels + enemies)
 
-def get_accessible_neighbors(cell, obstacles=None, allow_obstacles=False, allow_enemy=False, allow_closed_gate=False, allow_stay=False):
+def get_accessible_neighbors(cell, obstacles=None, place=False, allow_obstacles=False, allow_enemy=False, allow_closed_gate=False, allow_stay=False):
 	neighbors = []
 	if ALLOW_DIAGONAL_MOVES and False:
 		directions = ((-1, -1), (0, -1), (+1, -1), (-1, 0), (+1, 0), (-1, +1), (0, +1), (+1, +1))
@@ -338,19 +338,19 @@ def get_accessible_neighbors(cell, obstacles=None, allow_obstacles=False, allow_
 		neigh = apply_diff(cell, diff)
 		if is_cell_in_room(neigh) and (
 			allow_closed_gate and game.map[neigh] == CELL_GATE0 or
-			is_cell_accessible(neigh, obstacles, allow_obstacles=allow_obstacles, allow_enemy=allow_enemy)
+			is_cell_accessible(neigh, obstacles, place, allow_obstacles=allow_obstacles, allow_enemy=allow_enemy)
 		):
 			neighbors.append(neigh)
 	debug(3, "* get_accessible_neighbors %s - %s" % (str(cell), neighbors))
 	return neighbors
 
-def get_accessible_cells(start_cell, obstacles=None):
+def get_accessible_cells(start_cell, obstacles=None, place=False):
 	accessible_cells = []
 	unprocessed_cells = [start_cell]
 	while unprocessed_cells:
 		cell = unprocessed_cells.pop(0)
 		accessible_cells.append(cell)
-		neigbours = get_accessible_neighbors(cell, obstacles)
+		neigbours = get_accessible_neighbors(cell, obstacles, place)
 		for n in neigbours:
 			if n not in accessible_cells and n not in unprocessed_cells:
 				unprocessed_cells.append(n)

constants.py
@@ -37,7 +37,7 @@ CELL_FLOOR_TYPES_FREQUENT = (*CELL_FLOOR_TYPES_RANDGEN, *((CELL_FLOOR,) * EMPTY_
 CELL_FLOOR_TYPES = (*CELL_FLOOR_TYPES_RANDGEN, CELL_FLOOR)
 CELL_FLOOR_EXTENSIONS = (*CELL_FLOOR_TYPES_RANDGEN, CELL_PLATE, CELL_START, CELL_FINISH, CELL_PORTAL, CELL_GATE0, CELL_GATE1, CELL_LOCK1, CELL_LOCK2, CELL_ODIRL, CELL_ODIRR, CELL_ODIRU, CELL_ODIRD, CELL_GLASS, CELL_TRAP0, CELL_TRAP1, CELL_BEAMGN, CELL_BEAMCL)
 CELL_ENEMY_PLACE_OBSTACLES = (*CELL_WALL_TYPES, CELL_PORTAL, CELL_START, CELL_FINISH, CELL_GATE0, CELL_GATE1, CELL_SAND, CELL_LOCK1, CELL_LOCK2, CELL_VOID, CELL_GLASS, CELL_TRAP0, CELL_TRAP1, CELL_BEAMGN, CELL_BEAMCL)
-CELL_CHAR_PLACE_OBSTACLES = (*CELL_WALL_TYPES, CELL_PLATE, CELL_PORTAL, CELL_GATE0, CELL_GATE1, CELL_SAND, CELL_LOCK1, CELL_LOCK2, CELL_VOID, CELL_GLASS, CELL_TRAP0, CELL_TRAP1, CELL_BEAMGN, CELL_BEAMCL)
+CELL_CHAR_PLACE_OBSTACLES = (*CELL_WALL_TYPES, CELL_PLATE, CELL_FINISH, CELL_PORTAL, CELL_GATE0, CELL_GATE1, CELL_SAND, CELL_LOCK1, CELL_LOCK2, CELL_VOID, CELL_GLASS, CELL_TRAP0, CELL_TRAP1, CELL_BEAMGN, CELL_BEAMCL)
 CELL_CHAR_MOVE_OBSTACLES  = (*CELL_WALL_TYPES, CELL_GATE0, CELL_LOCK1, CELL_LOCK2, CELL_VOID, CELL_ODIRL, CELL_ODIRR, CELL_ODIRU, CELL_ODIRD, CELL_GLASS)
 
 ACTOR_CHARS = {

commit 691b72a6079c62a3ff9fd7f40072656188b13453

Fix temporarily broken find_path and get_accessible_cell_distances
Fix the usage of get_accessible_neighbors in some places, since the new
parameter was inserted in the middle.

Fixes: fd89f12a26ae ("Add place flag to get_accessible_cells and get_accessible_neighbors")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

main.py
@@ -364,7 +364,7 @@ def get_accessible_cell_distances(start_cell, obstacles=None, allow_obstacles=Fa
 		cell = unprocessed_cells.pop(0)
 		accessible_distance = accessible_cell_distances[cell]
 		accessible_cells.append(cell)
-		neigbours = get_accessible_neighbors(cell, obstacles, allow_obstacles, allow_enemy)
+		neigbours = get_accessible_neighbors(cell, obstacles, allow_obstacles=allow_obstacles, allow_enemy=allow_enemy)
 		for n in neigbours:
 			if n not in accessible_cells and n not in unprocessed_cells:
 				unprocessed_cells.append(n)
@@ -404,7 +404,7 @@ def find_path(start_cell, target_cell, obstacles=None, allow_obstacles=False, al
 	path_cells = [target_cell]
 	while accessible_distance > 1:
 		accessible_distance -= 1
-		neigh_cells = get_accessible_neighbors(path_cells[0], obstacles, allow_obstacles, allow_enemy)
+		neigh_cells = get_accessible_neighbors(path_cells[0], obstacles, allow_obstacles=allow_obstacles, allow_enemy=allow_enemy)
 		if randomize:
 			shuffle(neigh_cells)
 		for neigh_cell in neigh_cells:
@@ -427,7 +427,7 @@ def find_all_paths(start_cell, target_cell, obstacles=None, allow_obstacles=Fals
 		accessible_distance -= 1
 		new_all_path_cells = []
 		for path_cells in all_path_cells:
-			neigh_cells = [cell for cell in get_accessible_neighbors(path_cells[0], obstacles, allow_obstacles)
+			neigh_cells = [cell for cell in get_accessible_neighbors(path_cells[0], obstacles, allow_obstacles=allow_obstacles)
 				if accessible_cell_distances.get(cell) == accessible_distance]
 			for neigh_cell in neigh_cells:
 				new_all_path_cells.append((neigh_cell, *path_cells))
@@ -467,7 +467,7 @@ def find_best_path(start_cell, target_cell, obstacles=None, allow_obstacles=Fals
 			target_cell_state = cell_state
 			break
 
-		neigbours = get_accessible_neighbors(cell, obstacles, allow_obstacles, allow_stay=allow_stay)
+		neigbours = get_accessible_neighbors(cell, obstacles, allow_obstacles=allow_obstacles, allow_stay=allow_stay)
 		if randomize:
 			shuffle(neigbours)
 		for neigh in neigbours: