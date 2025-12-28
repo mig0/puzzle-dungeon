mig0/puzzle-dungeon

commit 84b85627c7d9c4493e2824cb0c08744041ae69a5

puzzle/barrel: Show Move Record and Push Record if exist
In BarrelPuzzle support sok files with Move-Record and/or Push-Record by
appending the records (if found in config) to "This is Sokoban puzzle"
status string.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/barrel.py
@@ -86,9 +86,14 @@ class BarrelPuzzle(Puzzle):
 		grid.check_zsb()
 		reverse_str = " reverse" if flags.is_reverse_barrel else ""
 		if grid.is_zsb:
-			msg = "This is Zero Space type-B %s%s puzzle!" % (grid.get_zsb_size_str(), reverse_str)
+			msg = "This is Zero Space type-B %s%s puzzle" % (grid.get_zsb_size_str(), reverse_str)
 		else:
 			msg = "This is Sokoban%s puzzle" % reverse_str
+		if "move-record" in self.config:
+			msg += " · Move Record: %s" % self.config["move-record"]
+		if "push-record" in self.config:
+			msg += " · Push Record: %s" % self.config["push-record"]
+
 		set_status_message(msg, self)
 
 	def on_enter_room(self):

commit cc693b15ec27be7c956c7de4177b698a8216a281

BarrelPuzzle: Only show Move and Push Record when not reverse mode
The Move and Push Record info included in the level file are only valid
for forward mode, and not for reverse mode. So do not show it on reverse.

Fixes: 84b85627c7d9 ("puzzle/barrel: Show Move Record and Push Record if exist")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/barrel.py
@@ -89,10 +89,11 @@ class BarrelPuzzle(Puzzle):
 			msg = "This is Zero Space type-B %s%s puzzle" % (grid.get_zsb_size_str(), reverse_str)
 		else:
 			msg = "This is Sokoban%s puzzle" % reverse_str
-		if "move-record" in self.config:
-			msg += " · Move Record: %s" % self.config["move-record"]
-		if "push-record" in self.config:
-			msg += " · Push Record: %s" % self.config["push-record"]
+		if not flags.is_reverse_barrel:
+			if "move-record" in self.config:
+				msg += " · Move Record: %s" % self.config["move-record"]
+			if "push-record" in self.config:
+				msg += " · Push Record: %s" % self.config["push-record"]
 
 		set_status_message(msg, self)
 