mig0/puzzle-dungeon

commit 88246a9705c4ff74bea41d75301d900cac5aaacf

Improve generate_random_free_path
Add to new params to generate_random_free_path:
	start_cell - use it instead of hardcoded char.c
	area - (optional) use it instead of room to generate path

Also, do some other improvements on the way, f.e. only use cell_distance
with 2 cell params instead of 4 numbers.

Modify BarrelPuzzle and StoneagePuzzle to use the new API.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/barrel.py
@@ -431,7 +431,7 @@ class BarrelPuzzle(Puzzle):
 			old_char_c = char.c
 			if char.c is None:
 				char.c = (cx, cy)
-			if self.Globals.generate_random_free_path(neighbor):
+			if self.Globals.generate_random_free_path(char.c, neighbor):
 				# pull the barrel to the char
 				barrel.c = char.c
 				char.c = (new_char_cx, new_char_cy)

main.py
@@ -481,8 +481,11 @@ def enter_room(idx):
 
 	puzzle.on_enter_room()
 
+def get_max_area_distance(area):
+	return cell_distance((area.x1, area.y1), (area.x2, area.y2))
+
 def get_max_room_distance():
-	return cell_distance((room.x1, room.y1), (room.x2, room.y2))
+	return get_max_area_distance(room)
 
 def is_actor_in_room(actor):
 	assert_room()
@@ -766,32 +769,32 @@ def generate_spiral_maze():
 def generate_random_maze_room():
 	generate_random_maze_area(room.x1, room.y1, room.x2, room.y2)
 
-def generate_random_free_path(target_c, deviation=0, level=0):
+def generate_random_free_path(start_cell, target_cell, area=None, deviation=0, level=0):
 	global map
 
 	if randint(0, deviation) == 0:
-		place_char_in_closest_accessible_cell(target_c)
+		start_cell = get_closest_accessible_cell(start_cell, target_cell)
 
-	if char.c == target_c:
+	if start_cell == target_cell:
 		return True
 
-	ox, oy = char.c
-	tx, ty = target_c
+	if area == None:
+		area = room
 
-	debug(2, "* [%d] generating free path from (%d, %d) to (%d, %d)" % (level, ox, oy, tx, ty))
+	debug_path_str = "free path from %s to %s" % (str(start_cell), str(target_cell))
+	debug(2, "* [%d] generating %s" % (level, debug_path_str))
 
-	max_distance = get_max_room_distance()
+	max_distance = get_max_area_distance(area)
 
-	accessible_cells = get_all_accessible_cells()
+	accessible_cells = get_accessible_cells(start_cell)
 	weighted_neighbors = []
-	for cell in get_actor_neighbors(char, room.x_range, room.y_range):
+	for cell in get_cell_neighbors(start_cell, area.x_range, area.y_range):
 		if cell in accessible_cells:
 			continue
 		if is_cell_in_actors(cell, barrels):
 			continue
-		cx, cy = cell
 		weight = randint(0, max_distance)
-		weight -= cell_distance(cx, cy, tx, ty)
+		weight -= cell_distance(cell, target_cell)
 		if map[cell] in CELL_FLOOR_TYPES:
 			weight -= randint(0, max_distance)
 		weighted_neighbors.append((weight, cell))
@@ -799,7 +802,7 @@ def generate_random_free_path(target_c, deviation=0, level=0):
 	neighbors = [n[1] for n in sorted(weighted_neighbors, reverse=True)]
 
 	if not neighbors:
-		debug(2, "* [%d] failed to generate free path from (%d, %d) to (%d, %d)" % (level, ox, oy, tx, ty))
+		debug(2, "* [%d] failed to generate %s" % (level, debug_path_str))
 		return False
 
 	for neigh in neighbors:
@@ -807,19 +810,16 @@ def generate_random_free_path(target_c, deviation=0, level=0):
 		if old_cell_type not in (*CELL_WALL_TYPES, CELL_VOID):
 			print("BUG!")
 			return False
-		convert_to_floor_if_needed(*neigh)
-		char.c = neigh
+		convert_to_floor_if_needed(neigh)
 		debug(3, "* [%d] trying to move to %s" % (level, str(neigh)))
-		debug_map(3, full=True, clean=True, combined=True)
-		is_path_found = generate_random_free_path(target_c, deviation, level + 1)
-		if is_path_found:
-			debug(2, "* [%d] successfully generated free path from (%d, %d) to (%d, %d)" % (level, ox, oy, tx, ty))
+		debug_map(3)
+		is_generated = generate_random_free_path(neigh, target_cell, area, deviation, level + 1)
+		if is_generated:
+			debug(2, "* [%d] successfully generated %s" % (level, debug_path_str))
 			if level == 0:
-				debug_map(2, full=True, clean=True, combined=True)
+				debug_map(2)
 			return True
 		map[neigh] = old_cell_type
-
-	char.c = (ox, oy)
 
 	return False
 

puzzle/stoneage.py
@@ -57,8 +57,7 @@ class StoneagePuzzle(Puzzle):
 		self.Globals.set_char_cell(start_cell)
 
 		self.Globals.replace_random_floor_cell(CELL_VOID, (self.room.x2 - self.room.x1 + 1) * (self.room.y2 - self.room.y1 + 1) - 2)
-		char.c = start_cell
-		self.Globals.generate_random_free_path(finish_cell, deviation=4)
+		self.Globals.generate_random_free_path(start_cell, finish_cell, deviation=4)
 
 		path_cells = self.Globals.find_path(start_cell, finish_cell)[:-1]
 		for i in range(int(randint(0, 60) * len(path_cells) / 100), int(randint(40, 100) * len(path_cells) / 100)):

commit fea79718462252e75fdd03c45607313d47dd0596

BarrelPuzzle: Fix old-generation level generation without use_ng
After successful generate_random_free_path the intention is to put barrel
into its neighbour (char_cell) and not into old char_cell as it was after
the commit mentioned in Fixes.

Also, pass area to generate_random_free_path, so that no floor outside
the area is replaced.

Also, rework the whole pull_barrel_randomly to use a new style with cells
and pull terminology.

Fixes: 88246a9705c4 ("Improve generate_random_free_path")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

puzzle/barrel.py
@@ -145,67 +145,60 @@ class BarrelPuzzle(Puzzle):
 
 	def pull_barrel_randomly(self, barrel, visited_cell_pairs, num_moves):
 		idx = barrels.index(barrel)
-		weighted_neighbors = []
+		weighted_pulls = []
 		# sort 4 barrel directions to place char to the "adjacent to barrel" cell for a pull (prefer empty cells)
-		for c in self.Globals.get_actor_neighbors(barrel, self.area.x_range, self.area.y_range):
-			if (c, char.c) in visited_cell_pairs:
+		for char_cell in self.Globals.get_actor_neighbors(barrel, self.area.x_range, self.area.y_range):
+			if (char_cell, char.c) in visited_cell_pairs:
 				continue
-			cx, cy = c
-			if is_cell_in_actors(c, barrels):
+			if is_cell_in_actors(char_cell, barrels):
 				continue
-			new_cx = cx + cx - barrel.cx
-			new_cy = cy + cy - barrel.cy
-			if new_cx not in self.area.x_range or new_cy not in self.area.y_range:
+			new_char_cell = apply_diff(char_cell, apply_diff(char_cell, barrel.c, subtract=True))
+			if not self.area.is_cell_inside(new_char_cell):
 				continue
-			if is_cell_in_actors((new_cx, new_cy), barrels):
+			if is_cell_in_actors(new_char_cell, barrels):
 				continue
 			weight = randint(0, 30)
-			if self.map[cx, cy] not in CELL_WALL_TYPES:
+			if self.map[char_cell] not in CELL_WALL_TYPES:
 				weight += 20
-			if self.map[cx, cy] == CELL_PLATE:
+			if self.map[char_cell] == CELL_PLATE:
 				weight += 4
-			if self.map[new_cx, new_cy] not in CELL_WALL_TYPES:
+			if self.map[new_char_cell] not in CELL_WALL_TYPES:
 				weight += 10
-			if self.map[new_cx, new_cy] == CELL_PLATE:
+			if self.map[new_char_cell] == CELL_PLATE:
 				weight += 2
-			weighted_neighbors.append((weight, c))
+			weighted_pulls.append((weight, (char_cell, new_char_cell)))
 
-		neighbors = [n[1] for n in sorted(weighted_neighbors, reverse=True)]
+		pulls = [n[1] for n in sorted(weighted_pulls, reverse=True)]
 
-		if not neighbors:
+		if not pulls:
 			# can't find free neighbor for barrel, stop
 			debug(2, "barrel #%d - failed to find free neighbor for barrel %s (%d left)" % (idx, barrel.c, num_moves))
 			return False
 
-		for neighbor in neighbors:
-			cx, cy = neighbor
-
+		for char_cell, new_char_cell in pulls:
 			# if the cell is not empty (WALL), make it empty (FLOOR with additions)
 			was_wall1_replaced = False
-			if self.map[neighbor] == CELL_WALL:
-				self.convert_to_floor(neighbor)
+			if self.map[char_cell] == CELL_WALL:
+				self.convert_to_floor(char_cell)
 				was_wall1_replaced = True
-			barrel_cx = barrel.cx
-			barrel_cy = barrel.cy
-			new_char_cx = cx + (cx - barrel_cx)
-			new_char_cy = cy + (cy - barrel_cy)
-			debug(2, "barrel #%d - neighbor %s, next cell (%d, %d)" % (idx, neighbor, new_char_cx, new_char_cy))
+			old_barrel_c = barrel.c
+			debug(2, "barrel #%d - neighbor %s, next cell %s" % (idx, char_cell, new_char_cell))
 			self.Globals.debug_map(2, full=True, clean=True, dual=True)
 			was_wall2_replaced = False
-			if self.map[new_char_cx, new_char_cy] == CELL_WALL:
-				self.convert_to_floor((new_char_cx, new_char_cy))
+			if self.map[new_char_cell] == CELL_WALL:
+				self.convert_to_floor(new_char_cell)
 				was_wall2_replaced = True
 
 			# if the char position is not None, first create random free path to the selected adjacent cell
 			old_char_c = char.c
 			if char.c is None:
-				char.c = (cx, cy)
-			if self.Globals.generate_random_free_path(char.c, neighbor):
+				char.c = char_cell
+			if self.Globals.generate_random_free_path(char.c, char_cell, area=self.area):
 				# pull the barrel to the char
-				barrel.c = char.c
-				char.c = (new_char_cx, new_char_cy)
+				barrel.c = char_cell
+				char.c = new_char_cell
 
-				visited_cell_pairs.append((neighbor, char.c))
+				visited_cell_pairs.append((char_cell, char.c))
 
 				if num_moves <= 1:
 					return True
@@ -215,15 +208,15 @@ class BarrelPuzzle(Puzzle):
 				else:
 					debug(2, "barrel #%d - failed to pull barrel (%d moves left)" % (idx, num_moves - 1))
 			else:
-				debug(2, "barrel #%d - failed to generate random free path to neighbor %s" % (idx, neighbor))
+				debug(2, "barrel #%d - failed to generate random free path to neighbor %s" % (idx, char_cell))
 
 			# can't create free path for char or can't pull barrel, restore the original state
 			char.c = old_char_c
-			barrel.c = (barrel_cx, barrel_cy)
+			barrel.c = old_barrel_c
 			if was_wall1_replaced:
-				self.map[cx, cy] = CELL_WALL
+				self.map[char_cell] = CELL_WALL
 			if was_wall2_replaced:
-				self.map[new_char_cx, new_char_cy] = CELL_WALL
+				self.map[new_char_cell] = CELL_WALL
 
 		return False
 