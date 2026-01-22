mig0/puzzle-dungeon

commit aa0491982b31400f263358dfa8186ab24580ef3a

Support importing solution from clipboard
This works best for BarrelPuzzle, since it currently supports importing
string consisting from lower and upper cased direction chars. But the
idea is generic.

Now Alt-S fills the solution from the clipboard (if any).

Second Alt-S (or KP-Enter, as usual) re-plays the active found solution.

Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

solution.py
@@ -161,6 +161,22 @@ class Solution:
 		current_cell_ref = [char.c]
 		return ''.join(item.get_str(current_cell_ref) for item in self.solution_items)
 
+	def set_str(self, solution_str):
+		args = []
+		for ch in solution_str:
+			if ch.isspace():
+				continue
+			if ch in DIRECTIONS or ch in SHIFT_DIRECTIONS:
+				args.append(ch)
+			else:
+				warn("Unsupported char (%s) is solution string")
+				return False
+		if not args:
+			warn("Ignored empty solution string")
+			return False
+		self.set(args)
+		return True
+
 	def set(self, args):
 		self.find_mode = False
 		self.solution_items = [item for item in (SolutionItem(arg) for arg in args) if not item.is_done]

main.py
@@ -1569,6 +1569,18 @@ def handle_press_key():
 	if mode == "next":
 		return
 
+	# if any key is pressed while playing solution, stop it
+	if cancel_playing_solution():
+		return
+
+	# if any key is pressed while finding solution, mark it as stop-find
+	if stop_finding_solution():
+		return
+
+	# unset solution explicitely with Backspace
+	if keyboard.backspace and unset_prepared_solution():
+		return
+
 	if keyboard.space and not cursor.is_active():
 		if game.map[char.c] == CELL_PORTAL:
 			teleport_char()
@@ -1598,6 +1610,12 @@ def handle_press_key():
 		if level_configs and not game.set_custom_collection_level_configs(level_configs):
 			warn("Failed to activate levels in clipboard")
 
+	if keyboard.s and keyboard.alt:
+		solution_str = clipboard.get()
+		if solution.is_active() and solution.get_str() == solution_str:
+			keyboard.kp_enter = True
+		solution.set_str(solution_str)
+
 	if cursor_was_active and (cursor_had_status_message or debug.lvl > 0) and not cursor.is_active():
 		set_status_message(priority=0)
 
@@ -1619,18 +1637,6 @@ def handle_press_key():
 		return
 
 	if not game.level.is_set():
-		return
-
-	# if any key is pressed while playing solution, stop it
-	if cancel_playing_solution():
-		return
-
-	# if any key is pressed while finding solution, mark it as stop-find
-	if stop_finding_solution():
-		return
-
-	# unset solution explicitely with Backspace
-	if keyboard.backspace and unset_prepared_solution():
 		return
 
 	if keyboard.p:

commit 8b81683792092755a00a625d7197a903aaf7c806

solution: Fix importing bad solution string
The GUI failed on Alt-S with bad chars due to invalid warn call. Fix it.

Fixes: aa0491982b31 ("Support importing solution from clipboard")
Signed-off-by: Mikhael Goikhman <migo@freeshell.org>

solution.py
@@ -1,5 +1,6 @@
 from time import time
 from flags import flags
+from common import warn
 from config import SOLUTION_MOVE_DELAY, SOLUTION_MOVE_DELAY_RANGE, SOLUTION_MOVE_DELAY_CHANGE
 from objects import char
 from constants import DIR_NAMES, DIRS_BY_NAME, DIRECTIONS
@@ -81,7 +82,7 @@ class SolutionItem:
 			self.is_done = not self.path_cells
 
 		if self.is_done:
-			print("Warning: Called play_move on SolutionItem that is done; ignoring")
+			warn("Called play_move on SolutionItem that is done; ignoring")
 			return
 
 		if self.cell_to_press:
@@ -169,7 +170,7 @@ class Solution:
 			if ch in DIRECTIONS or ch in SHIFT_DIRECTIONS:
 				args.append(ch)
 			else:
-				warn("Unsupported char (%s) is solution string")
+				warn("Unsupported char '%s' in solution string" % ch)
 				return False
 		if not args:
 			warn("Ignored empty solution string")